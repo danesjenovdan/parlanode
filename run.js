0c9a71ec3d9857f31425c992c29612aa6809813d
FranciZ <franci@proxima.si>
2016-02-27 16:46:32 +0100

    Changed production port

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ var server      = require('./server/server');
 var database    = require('./server/database');
 var Promise     = require('bluebird');
 
-GLOBAL.CFG = require('./config').dev;
+GLOBAL.CFG = require('./config').production;
 
 function init(){
 
