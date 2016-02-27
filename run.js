23dfd4973e85af838f3e1afeead16f1530533215
FranciZ <franci@proxima.si>
2016-02-27 16:23:59 +0100

    Test deploy

--- run.js
+++ run.js
@@ -1,11 +1,10 @@
-var server = require('./server/server');
-var database = require('./server/database');
-var Promise = require('bluebird');
+var server      = require('./server/server');
+var database    = require('./server/database');
+var Promise     = require('bluebird');
 
 GLOBAL.CFG = require('./config').dev;
 
 function init(){
-
 
     database.connect()
         .then(server.init)
@@ -13,9 +12,7 @@ function init(){
 
         });
 
-
 }
-
 
 init();
 
