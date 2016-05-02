0e576c79aed9820c429c81c8e6ea52e285e97627
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-05-02 20:11:22 +0200

    removing duplicates

--- assets/embed.script.js
+++ assets/embed.script.js
@@ -1,75 +1,86 @@
+if (!window.parlaCardInit) {
 
-        if(!window.parlaCardInit) {
+    window.parlaCardInit = true;
 
-            window.parlaCardInit = true;
+    function loadScript(url, callback) {
 
-            function loadScript(url, callback) {
+        // check for duplicates
+        var list = document.getElementsByTagName('script');
+        var i = list.length,
+            flag = false;
+        while (i--) {
+            if (list[i].src == url) {
+                flag = true;
+            }
+        }
+        if (!flag) {
+            var tag = document.createElement('script');
+            tag.type = "text/javascript";
 
-                var script = document.createElement("script")
-                script.type = "text/javascript";
-
-                if (script.readyState) { //IE
-                    script.onreadystatechange = function () {
-                        if (script.readyState == "loaded" || script.readyState == "complete") {
-                            script.onreadystatechange = null;
-                            callback();
-                        }
-                    };
-                } else { //Others
-                    script.onload = function () {
+            if (script.readyState) { //IE
+                script.onreadystatechange = function() {
+                    if (script.readyState == "loaded" || script.readyState == "complete") {
+                        script.onreadystatechange = null;
                         callback();
-                    };
-                }
-
-                script.src = url;
-                document.getElementsByTagName("head")[0].appendChild(script);
+                    }
+                };
+            } else { //Others
+                script.onload = function() {
+                    callback();
+                };
             }
 
-            loadScript("https://code.jquery.com/jquery-1.11.3.js", function () {
-                loadScript('https://cdn.parlameter.si/v1/parlassets/js/perfect-scrollbar.js', function() {
-                    loadScript('https://cdn.parlameter.si/v1/parlassets/js/perfect-scrollbar.jquery.js', function() {
-                        loadScript('https://d3js.org/d3.v3.min.js', function() {
-                            loadScript('https://cdn.parlameter.si/v1/parlassets/js/script.js', function() {
-                                init();
-                            });
-                        });
+            tag.src = url;
+            document.getElementsByTagName("head")[0].appendChild(tag);
+        }
+
+    }
+
+    loadScript("https://code.jquery.com/jquery-1.11.3.js", function() {
+        loadScript('https://cdn.parlameter.si/v1/parlassets/js/perfect-scrollbar.js', function() {
+            loadScript('https://cdn.parlameter.si/v1/parlassets/js/perfect-scrollbar.jquery.js', function() {
+                loadScript('https://d3js.org/d3.v3.min.js', function() {
+                    loadScript('https://cdn.parlameter.si/v1/parlassets/js/script.js', function() {
+                        init();
                     });
                 });
             });
+        });
+    });
 
-            function init() {
+    function init() {
 
-                var $cards = $('[data-parlameter="card"]');
+        var $cards = $('[data-parlameter="card"]');
 
-                for (var i = 0; i < $cards.length; i++) {
+        for (var i = 0; i < $cards.length; i++) {
 
-                    var card = new Card($cards[i]);
-
-                }
-
-
-            }
-
-            var Card = function (element) {
-
-                this.mUrl = $(element).attr('data-src');
-                this.mElement = $(element);
-
-                this.fetchCard();
-
-            };
-
-            Card.prototype.fetchCard = function () {
-
-                var self = this;
-
-                $.get(this.mUrl)
-                        .success(function (response) {
-
-                            self.mElement.append(response);
-
-                        });
-
-            };
+            var card = new Card($cards[i]);
 
         }
+
+
+    }
+
+    var Card = function(element) {
+
+        this.mUrl = $(element).attr('data-src');
+        this.mElement = $(element);
+
+        this.fetchCard();
+
+    };
+
+    Card.prototype.fetchCard = function() {
+
+        var self = this;
+
+        $.get(this.mUrl)
+            .success(function(response) {
+
+                self.mElement.append(response);
+
+            });
+
+    };
+
+}
