6332f387e367fb23ff55862f98de0d9450302650
FranciZ <franci@proxima.si>
2016-08-15 19:52:30 +0200

    fix url?

--- server/resources/auth/routes.js
+++ server/resources/auth/routes.js
@@ -3,8 +3,8 @@ const authMiddleware = require('../../middlewares/auth');
 
 module.exports = (app)=>{
 
-    app.post(`${CFG.apiPrefix}/login`, controller.login);
+    app.post('api/login', controller.login);
 
-    app.post(`${CFG.apiPrefix}/checkLogin`, authMiddleware, controller.isLoggedIn);
+    app.post('api/checkLogin', authMiddleware, controller.isLoggedIn);
 
 };
