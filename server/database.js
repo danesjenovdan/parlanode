9073284133dc0a33e89b97a5cff1c26f0f2648f2
FranciZ <franci@proxima.si>
2016-11-11 21:11:42 +0100

    db update;

--- server/database.js
+++ server/database.js
@@ -8,7 +8,7 @@ exports.connect = function(cb){
 
     return new Promise(function(resolve, reject){
 
-        mongoose.connect('admin:mypassword@ds047945.mongolab.com:47945/heroku_72h76cfr');
+        mongoose.connect('http://localhost/parladb');
         //mongoose.connect(CFG.db.url+CFG.db.name);
 
         mongoose.connection.on('error', function(err){
