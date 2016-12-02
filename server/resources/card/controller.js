77c6afa41c0a6127f222d108aac15f8e29f0e61b
FranciZ <franci@proxima.si>
2016-12-02 15:05:35 +0100

    fix?;

--- server/resources/card/controller.js
+++ server/resources/card/controller.js
@@ -437,7 +437,7 @@ exports.render = function (req, res) {
                     }
                   } catch (err) {
                     console.log('Err:2 ',err);
-                    res.status(400).send(err);
+                    res.status(400).send(err.stack);
                   }
 
                 } else {
