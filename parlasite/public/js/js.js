cdf949e071aeeb3eec5205fcbec991f4c31a36a4
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-12-15 17:32:06 +0100

    safari search fix

--- parlasite/public/js/js.js
+++ parlasite/public/js/js.js
@@ -183,8 +183,8 @@ $(function () {
         }
 
         function encodeQueryData(data) {
-            let ret = [];
-            for (let d in data)
+            var ret = [];
+            for (var d in data)
                 //ret.push(encodeURIComponent(d) + '/' + encodeURIComponent(data[d]));
                 ret.push((d) + '/' + encodeURIComponent(data[d]));
             return ret.join('&');
