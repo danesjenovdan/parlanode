8eeee9901064d2c7f5952f8eed98fbc7f8aba013
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-01-13 12:47:42 +0100

    added alphanumSort

--- parlassets/js/script.js
+++ parlassets/js/script.js
@@ -628,3 +628,80 @@ function measure(c,a,n,v) {
     _paq.push(['trackEvent', c, a]);
     return true;
 }
+
+/* ********************************************************************
+ * Alphanum Array prototype version
+ *  - Much faster than the sort() function version
+ *  - Ability to specify case sensitivity at runtime is a bonus
+ *
+ */
+Array.prototype.alphanumSort = function(caseInsensitive) {
+  for (var z = 0, t; t = this[z]; z++) {
+    this[z] = new Array();
+    var x = 0, y = -1, n = 0, i, j;
+
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
+      if (m !== n) {
+        this[z][++y] = "";
+        n = m;
+      }
+      this[z][y] += j;
+    }
+  }
+
+  this.sort(function(a, b) {
+    for (var x = 0, aa, bb; (aa = a[x]) && (bb = b[x]); x++) {
+      if (caseInsensitive) {
+        aa = aa.toLowerCase();
+        bb = bb.toLowerCase();
+      }
+      if (aa !== bb) {
+        var c = Number(aa), d = Number(bb);
+        if (c == aa && d == bb) {
+          return c - d;
+        } else return (aa > bb) ? 1 : -1;
+      }
+    }
+    return a.length - b.length;
+  });
+
+  for (var z = 0; z < this.length; z++)
+    this[z] = this[z].join("");
+}
+
+/* ********************************************************************
+ * Alphanum sort() function version - case insensitive
+ *  - Slower, but easier to modify for arrays of objects which contain
+ *    string properties
+ *
+ */
+function alphanumCase(a, b) {
+  function chunkify(t) {
+    var tz = new Array();
+    var x = 0, y = -1, n = 0, i, j;
+
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
+      if (m !== n) {
+        tz[++y] = "";
+        n = m;
+      }
+      tz[y] += j;
+    }
+    return tz;
+  }
+
+  var aa = chunkify(a.toLowerCase());
+  var bb = chunkify(b.toLowerCase());
+
+  for (x = 0; aa[x] && bb[x]; x++) {
+    if (aa[x] !== bb[x]) {
+      var c = Number(aa[x]), d = Number(bb[x]);
+      if (c == aa[x] && d == bb[x]) {
+        return c - d;
+      } else return (aa[x] > bb[x]) ? 1 : -1;
+    }
+  }
+  return aa.length - bb.length;
+}
