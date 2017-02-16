f4dead1f36d374a3c2b86f4e12a3511eda4f6877
FranciZ <franci@proxima.si>
2017-02-16 20:26:06 +0100

    sps

---
+++ parlasite/services/data-service.js
@@ -0,0 +1,57 @@
+const Promise = require('bluebird');
+const request = require('request');
+const CronJob = require('cron').CronJob;
+const fs      = require('fs');
+
+const spsFilePath = __dirname + '/../data/sps.json';
+
+exports.sps = {};
+
+exports.loadSPS = () => {
+
+  return Promise.resolve()
+    .then(() => {
+
+      const spsExists = fs.existsSync(spsFilePath);
+
+      if ( spsExists ) {
+        spsRequest();
+        return Promise.resolve(JSON.parse(fs.readFileSync(spsFilePath, 'UTF-8')));
+      }
+
+      return spsRequest();
+
+    })
+    .then(spsData => exports.sps = spsData);
+
+};
+
+function spsRequest() {
+
+  return new Promise(( resolve, reject ) => {
+
+    request('https://analize.parlameter.si/v1/s/getSessionsByClassification/', ( err, res, body ) => {
+
+      if ( err ) return reject(err);
+
+      try {
+        console.log(body);
+        fs.writeFileSync(spsFilePath, body);
+        resolve(JSON.parse(body));
+      }
+      catch ( err ) {
+        reject(err);
+      }
+
+    });
+
+  });
+
+}
+
+/**
+ * Cronjob refetching data every midnight
+ */
+new CronJob('00 4 * * *', () => {
+  spsRequest();
+}, null, true);
