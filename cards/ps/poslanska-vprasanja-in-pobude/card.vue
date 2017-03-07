0039a56529592c2abab6cfc87f5bdc4e73fff455
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-03-07 18:03:51 +0100

    muki stuff

---
+++ cards/ps/poslanska-vprasanja-in-pobude/card.vue
@@ -0,0 +1,15 @@
+<template>
+  <poslanska-vprasanja-in-pobude
+    :card-data="$options.cardData"
+    type="party"
+    :party="$options.cardData.data.party" />
+</template>
+
+<script>
+import PoslanskaVprasanjaInPobude from 'components/PoslanskaVprasanjaInPobude.vue'
+
+export default {
+  name: 'PoslanskaVprasanjaInPobudePoslanskeSkupine',
+  components: { PoslanskaVprasanjaInPobude }
+}
+</script>
