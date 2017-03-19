d428d461eb5c83b44c6dae9de4bbd1606bdc2fa1
mzgajner <mato@zgajner.com>
2017-03-19 13:27:18 +0100

    Finish implementing basic hot-reload config.

--- cards/webpack.config.dev.js
+++ cards/webpack.config.dev.js
@@ -2,19 +2,22 @@
 const webpack = require('webpack');
 const baseConfig = require('./webpack.config.base');
 
-module.exports = (cardPath) => {
-  const config = baseConfig(cardPath);
+module.exports = () => {
+  const config = baseConfig(`./cards/${process.env.npm_config_card}`);
   return Object.assign(config, {
-    entry: './cards/clientBundle.js',
+    entry: './cards/devBundle.js',
     output: {
       filename: 'bundle.js',
     },
-    plugins: config.plugins.concat([
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         },
       }),
-    ]),
+    ],
   });
 };
