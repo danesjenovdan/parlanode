96d3b792086f6e05c1ad8cdb116bfed2a5113776
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-06-16 14:28:16 +0200

    removed duplicate dev settings from config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -14,22 +14,6 @@ var dev = {
     cardCapturePath: './card_captures', // where cards are stored
 };
 
-var dev = {
-    port: 3000,
-    serverTimeout: 600000,
-    db: {
-        name: 'parla-db',
-        url: 'mongodb://localhost/',
-    },
-    urls: {
-        analize: 'http://localhost:8000', // DATA URL BASE
-    },
-    apiPrefix: '/api',
-    ogCapturePath: './og',
-    ogRootUrl: 'https://cdn.whatever.com/og_cards/', // root url for OG images
-    cardCapturePath: './card_captures', // where cards are stored
-};
-
 var production = {
     port: 3000,
     serverTimeout: 600000,
