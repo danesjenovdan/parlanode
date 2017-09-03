2e85167211b06287f01486a110dbf86e389cb54a
mzgajner <mato@zgajner.com>
2017-09-03 19:34:42 +0200

    Implement card rippling

--- cards/_mixins/common.js
+++ cards/_mixins/common.js
@@ -3,6 +3,7 @@ import CardWrapper from 'components/Card/Wrapper.vue';
 import initializeBack from 'mixins/initializeBack';
 import url from 'mixins/url';
 import slugs from '../../assets/urls.json';
+import { RIPPLE_DURATION } from 'components/constants';
 
 export default {
   data() {
@@ -17,4 +18,27 @@ export default {
     initializeBack,
     url,
   ],
+  methods: {
+    toggleBack(newBack) {
+      const contentElement = this.$el.querySelector('.card-content');
+
+      if (this.currentBack !== newBack) {
+        this.transitionClass = ['covered', `clicked-${newBack}`];
+
+        window.setTimeout(() => {
+          contentElement.style.height = `${contentElement.offsetHeight}px`;
+          this.currentBack = newBack;
+        }, RIPPLE_DURATION / 2);
+        window.setTimeout(() => { this.transitionClass = null; }, RIPPLE_DURATION);
+      } else {
+        this.transitionClass = ['revealed', `clicked-${newBack}`];
+
+        window.setTimeout(() => {
+          this.currentBack = null;
+          contentElement.style.height = null;
+        }, RIPPLE_DURATION / 2);
+        window.setTimeout(() => { this.transitionClass = null; }, RIPPLE_DURATION);
+      }
+    },
+  },
 };
