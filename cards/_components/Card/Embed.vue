d15f32d94d6c5dbda71e2a08d429b0388d38ccb8
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-09-06 11:40:32 +0200

    removed makeEmbedSwitch()

--- cards/_components/Card/Embed.vue
+++ cards/_components/Card/Embed.vue
@@ -5,8 +5,8 @@
         <div class="embed-divider"></div>
         <div class="embed-switch-container">
           <div class="embed-label">Podatki naj se vedno osvežujejo</div>
-          <div class="embed-switch-big-box">
-            <div class="embed-switch-box">
+          <div class="embed-switch-big-box" @click="toggleEmbedSwitch">
+            <div :class="['embed-switch-box', { off: !this.refresh }]">
               <div class="embed-switch">
                 <div class="embed-switches">
                   <div class="leftswitch">DA</div>
@@ -30,13 +30,34 @@
 <script>
 export default {
   name: 'CardEmbed',
+
   computed: {
     embedCode() {
-      return `&#x3C;script&#x3E;(function(d,script){script=d.createElement(&#x27;script&#x27;);script.type=&#x27;text/javascript&#x27;;script.async=true;script.onload=function(){iFrameResize({log:true,checkOrigin:false})};script.src=&#x27;https://cdn.parlameter.si/v1/parlassets/js/iframeResizer.min.js&#x27;;d.getElementsByTagName(&#x27;head&#x27;)[0].appendChild(script);}(document));&#x3C;/script&#x3E;&#x3C;iframe frameborder=&#x22;0&#x22; width=&#x22;100%&#x22; src=&#x22;${this.url}&#x26;embed=true&#x22;&#x3E;&#x3C;/iframe&#x3E;`;
+      let newUrl = this.url;
+      if (!this.refresh) {
+        const todaysdate = new Date;
+        const today = '' + todaysdate.getDay() + '.' + todaysdate.getMonth() + '.' + todaysdate.getFullYear();
+        newUrl = `${this.url.split('?')[0]}${today}?${this.url.split('?')[1]}`;
+      }
+
+      return `&#x3C;script&#x3E;(function(d,script){script=d.createElement(&#x27;script&#x27;);script.type=&#x27;text/javascript&#x27;;script.async=true;script.onload=function(){iFrameResize({log:true,checkOrigin:false})};script.src=&#x27;https://cdn.parlameter.si/v1/parlassets/js/iframeResizer.min.js&#x27;;d.getElementsByTagName(&#x27;head&#x27;)[0].appendChild(script);}(document));&#x3C;/script&#x3E;&#x3C;iframe frameborder=&#x22;0&#x22; width=&#x22;100%&#x22; src=&#x22;${newUrl}&#x26;embed=true&#x22;&#x3E;&#x3C;/iframe&#x3E;`;
     },
   },
+
   props: {
     url: String,
+  },
+
+  data() {
+    return {
+      refresh: true,
+    };
+  },
+
+  methods: {
+    toggleEmbedSwitch() {
+      this.refresh = !this.refresh;
+    },
   },
 };
 </script>
