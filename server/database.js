2b0811855e4acce598092b0f55ed513b9012fc97
FranciZ <franci@proxima.si>
2017-09-30 18:41:00 +0200

    Set username and password when connecting to mongo

--- server/database.js
+++ server/database.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 const chalk    = require('chalk');
 const Promise  = require('bluebird');
+const config   = require('../config');
 
 /**
  * Connects to MongoDB
  * @param cb
  */
-exports.connect = function (cb) {
+exports.connect = function ( cb ) {
 
-  return new Promise(function (resolve, reject) {
+  return new Promise(function ( resolve, reject ) {
 
     console.log(chalk.magenta(`| MONGO DATABASE |`) + ` -` + chalk.green(` Connecting to ${CFG.db.url + CFG.db.name}`));
-    mongoose.connect(CFG.db.url + CFG.db.name);
 
-    mongoose.connection.on('error', function (err) {
+    mongoose.connect(CFG.db.url + CFG.db.name, {
+      user : config.db.user,
+      pass : config.db.password
+    });
+
+    mongoose.connection.on('error', function ( err ) {
 
       reject(err);
       console.log(chalk.red(err));
@@ -22,7 +27,7 @@ exports.connect = function (cb) {
 
     mongoose.connection.once('open', function () {
 
-      if (cb) {
+      if ( cb ) {
         cb();
       }
       console.log(chalk.green(chalk.magenta('| MONGO DATABASE |') + ' - connected'));
