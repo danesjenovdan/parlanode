3aae07d35bf8db545aa31529fc64a74071629aba
Matic Prevolsek <prevolsek@gmail.com>
2017-11-15 07:44:36 +0100

    new card ps/govori

---
+++ cards/ps/govori/card.vue
@@ -0,0 +1,19 @@
+<template>
+  <govori
+    :card-data="$options.cardData"
+    type="party"
+    :person="$options.cardData.data.party"
+  ></govori>
+</template>
+
+<script>
+import Govori from 'components/Govori.vue'
+
+export default {
+  components: { Govori },
+  name: 'GovoriPoslanskeSkupine',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
