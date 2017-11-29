3e1e1e3f5e68ce78d538047631d8161bbdddcdf3
Matic Prevolsek <prevolsek@gmail.com>
2017-11-29 23:40:40 +0100

    style filters and make them work, table styles

--- cards/p/seznam-zakonov/innerCard.vue
+++ cards/p/seznam-zakonov/innerCard.vue
@@ -1,6 +1,6 @@
 <template>
   <card-wrapper
-    class="card-halfling card-seznam-sej"
+    class="card-halfling card-seznam-zakonov"
     :card-url="generatedCardUrl"
     :header-config="headerConfig">
 
@@ -10,6 +10,7 @@
       <p class="info-text">Podatke o sejah pridobivamo iz spletnega mesta DZ RS, natančneje od <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDt/poDatumu/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3F0NXQw8DX09PTz9HI0M3E30w9EUBJkYARV4W4b4-PoYGnib6UdRot8dXT-G8Wj6_S0N3Qw8Q43dTYx9QwwMfI2I02-AAzgakGg_pgej8BtfkBsKAooAQ6b9bA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/">tu</a>, <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDrzavnegaZbora/PoDatumuSeje/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3INMjAw8Db0tQ3x8fQwNvM30wwkpiAJKG-AAjgYE9LtD9BNvv7-loZuBZ6ixu4mxb4iBga8RcfrxOJCA_oLcUCBwVAQAGc0QlQ!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/">tu</a> in <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDt/poDatumu/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3F0NXQw8DX09PTz9HI0M3E30w9EUBJkYARV4W4b4-PoYGnib6UdRot8dXT-G8Wj6_S0N3Qw8Q43dTYx9QwwMfI2I02-AAzgakGg_pgej8BtfkBsKAooAQ6b9bA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/.">tu.</a></p>
     </div>
 
+    <div class="row">
     <sortable-table
       class="session-list"
       :columns="columns"
@@ -18,6 +19,7 @@
       :sort-order="currentSortOrder"
       :sort-callback="selectSort"
     />
+    </div>
   </card-wrapper>
 </template>
 
@@ -44,9 +46,9 @@ export default {
   computed: {
       mappedItems () {
           return this.items.map(legislation => [
-              { link: '#', text: legislation.text },
+              { link: this.slugs.legislationLink + legislation.epa, text: legislation.text },
               { text: 'bla' },
-              { link: '#', text: legislation.mdt },
+              { link: `${ORGS_ROOT_URL}?frame=true&altHeader=true`, text: legislation.mdt },
               { html: '<b>' + legislation.result + '</b>' },
           ])
       }
@@ -59,3 +61,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .card-seznam-zakonov .session-list {
+    .headers { margin-left: 0; }
+  }
+</style>
