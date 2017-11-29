b0c3c5a7aebc248c011c0a441a25be07e8accee5
Matic Prevolsek <prevolsek@gmail.com>
2017-11-29 08:41:30 +0100

    init tools subpage

---
+++ parlasite/views/orodja/data.js
@@ -0,0 +1,32 @@
+export const getToools = [
+  {
+    icon  : 'obvestila',
+    title : 'Parlameter obvestila',
+    link  : '/orodja/parlameter-obvestila',
+    text  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    icon  : 'primerjalnik',
+    title : 'Primerjalnik glasovanj',
+    link  : '/orodja/primerjalnik-glasovanj',
+    text  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    icon  : 'raziskovalec',
+    title : 'Raziskovalec neenotnosti',
+    link  : '/orodja/raziskovalec-neenotnosti',
+    text  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    icon  : 'kompas',
+    title : 'Parlamentarni kompas',
+    link  : '/orodja/parlamentarni-kompas',
+    text  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    icon  : 'parlatube',
+    title : 'Parlatube',
+    link  : '/orodja/parlatube',
+    text  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+];
