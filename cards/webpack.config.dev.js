d5c993a710a420f872b81bf6a30a092b0ba5ec08
mzgajner <mato@zgajner.com>
2017-12-03 21:18:52 +0100

    Allow prefixing card paths with 'cards/'

--- cards/webpack.config.dev.js
+++ cards/webpack.config.dev.js
@@ -4,7 +4,13 @@ const webpack = require('webpack');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = () => {
-  const config = baseConfig(`${path.resolve(__dirname)}/${process.env.CARD_NAME}`);
+  let cardName = process.env.CARD_NAME;
+
+  if (cardName.indexOf('cards/') === 0) {
+    cardName = cardName.replace('cards/', '');
+  }
+
+  const config = baseConfig(`${path.resolve(__dirname)}/${cardName}`);
 
   return Object.assign(config, {
     entry: './cards/devBundle.js',
