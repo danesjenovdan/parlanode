1341fc8cee9263eb3b8a320809e9a54526478168
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-12-10 14:37:47 +0100

    switch card renderer to stage

--- parlasite/config.stage.js
+++ parlasite/config.stage.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   PORT:32279,
-  CARD_RENDERER_API_ROOT:'https://glej.parlameter.si',
+  CARD_RENDERER_API_ROOT:'https://knedl.si:3000',
   CARD_RENDERER_SEARCH_API_ROOT:'https://isci.parlameter.si',
-  CARD_RENDERER_ANALYSIS_API_ROOT:'https://parlalize.muzikasuper.si',
+  CARD_RENDERER_ANALYSIS_API_ROOT:'https://analize.parlameter.si',
   OG_ROOT_URL:'https://cdn.parlameter.si/v1/parlassets/og_cards/site/',
   OG_CAPTURE_PATH:'/home/djnd/djstatic/parlacdn/v1/parlassets/og_cards/site/'
 };
