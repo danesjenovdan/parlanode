950f08695e4b8f2cd6c21da5d5a46bee8cbd7739
Matic Prevolsek <prevolsek@gmail.com>
2017-12-13 14:52:45 +0100

    fix sorting and rebuild

--- cards/c/zakonodaja/innerCard.vue
+++ cards/c/zakonodaja/innerCard.vue
@@ -10,9 +10,7 @@
       <p class="info-text">Podatke o sejah pridobivamo iz spletnega mesta DZ RS, natančneje od <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDt/poDatumu/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3F0NXQw8DX09PTz9HI0M3E30w9EUBJkYARV4W4b4-PoYGnib6UdRot8dXT-G8Wj6_S0N3Qw8Q43dTYx9QwwMfI2I02-AAzgakGg_pgej8BtfkBsKAooAQ6b9bA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/">tu</a>, <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDrzavnegaZbora/PoDatumuSeje/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3INMjAw8Db0tQ3x8fQwNvM30wwkpiAJKG-AAjgYE9LtD9BNvv7-loZuBZ6ixu4mxb4iBga8RcfrxOJCA_oLcUCBwVAQAGc0QlQ!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/">tu</a> in <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDt/poDatumu/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zivT39gy2dDB0N3F0NXQw8DX09PTz9HI0M3E30w9EUBJkYARV4W4b4-PoYGnib6UdRot8dXT-G8Wj6_S0N3Qw8Q43dTYx9QwwMfI2I02-AAzgakGg_pgej8BtfkBsKAooAQ6b9bA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/.">tu.</a></p>
     </div>
 
-    <empty-card v-if="mappedItems.length === 0" text="Brez rezultatov"></empty-card>
     <sortable-table
-      v-else
       class="legislation-list"
       :columns="columns"
       :items="mappedItems"
@@ -24,8 +22,6 @@
 </template>
 
 <script>
-import EmptyCard from 'components/Card/Empty.vue';
-
 import SortableTable from 'components/SortableTable.vue';
 import common from 'mixins/common';
 import formatDate from 'helpers/dateFormatter';
@@ -34,7 +30,6 @@ import { ICONS_ROOT_URL, ORGS_ROOT_URL } from 'components/constants';
 export default {
   components: {
     SortableTable,
-    EmptyCard
   },
   mixins: [common],
   name: 'SeznamZakonovKartica',
@@ -92,10 +87,12 @@ export default {
 </script>
 
 <style lang="scss">
+  @import '~parlassets/scss/colors';
   @import '~parlassets/scss/breakpoints';
 
-  .card-content__empty {
-    padding-top: 0;
+  .empty-dataset {
+    color: $grey-medium;
+    font-style: normal !important;
   }
 
   .legislation-list {
