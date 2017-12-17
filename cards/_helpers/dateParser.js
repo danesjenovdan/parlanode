07855447bd132f7f079abf0985583dda10bec326
Matic Prevolsek <prevolsek@gmail.com>
2017-12-17 17:39:42 +0100

    fix dateParser.js month index

--- cards/_helpers/dateParser.js
+++ cards/_helpers/dateParser.js
@@ -1,7 +1,7 @@
 export default (stringDate) => {
   var d = new Date();
   d.setDate(parseInt(stringDate.split('.')[0], 10));
-  d.setMonth(parseInt(stringDate.split('.')[1], 10));
+  d.setMonth(parseInt(stringDate.split('.')[1], 10) - 1);
   d.setFullYear(parseInt(stringDate.split('.')[2], 10));
   return d;
 }
