ac7c0ee16f2cbfc3d7bd9abd521823a6133ff51d
Matic Prevolsek <prevolsek@gmail.com>
2017-12-21 22:12:51 +0100

    make all filters equal height

--- cards/s/seznam-glasovanj/card.vue
+++ cards/s/seznam-glasovanj/card.vue
@@ -465,7 +465,7 @@
         background-position: right 9px center;
         border: 1px solid #c8c8c8;
         font-size: 16px;
-        height: 51px;
+        height: 53px;
         line-height: 27px;
         outline: none;
         padding: 12px 42px 12px 14px;
