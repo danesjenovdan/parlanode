d427fa62bc93b10f3a1143c266628d57adfa4c6f
LepkoQQ <lepko.san@gmail.com>
2018-05-07 19:56:47 +0200

    add "p/besedni-zaklad" and "p/stilne-analize"

---
+++ cards/_components/StyleScores.vue
@@ -0,0 +1,170 @@
+<template>
+  <card-wrapper
+    :id="cardData.cardData._id"
+    class="card-halfling"
+    :data-id="`${cardData.cardData.group}/${cardData.cardData.method}`"
+    :card-url="generatedCardUrl"
+    :header-config="headerConfig"
+  >
+    <slot name="info" slot="info"></slot>
+
+    <div class="card-content-front" v-cloak>
+      <div class="progress_flex">
+        <div class="column-title progress_title">
+          <div class="me_poslanec">
+            <div class="poslanec_title">
+              Privzdignjeno besedje
+            </div>
+          </div>
+          <div class="me_poslanec">
+            <div class="poslanec_title">
+              Preprosto besedje
+            </div>
+          </div>
+          <div class="me_poslanec">
+            <div class="poslanec_title">
+              Ekscesno besedje
+            </div>
+          </div>
+        </div>
+        <div class="column-bar progress_bar">
+          <div class="me_poslanec">
+            <div class="progress smallbar">
+              <div class="progress-bar funblue" role="progressbar" :aria-valuenow="results.privzdignjeno" aria-valuemin="0" aria-valuemax="100" :style="getBarStyle('privzdignjeno')">
+                <span class="sr-only">{{ results.privzdignjeno.toFixed(2) }}%</span>
+                <div class="progress_number">
+                  {{ results.privzdignjeno.toFixed(2).replace('.', ',') }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="me_poslanec">
+            <div class="progress smallbar">
+              <div class="progress-bar funblue" role="progressbar" :aria-valuenow="results.preprosto" aria-valuemin="0" aria-valuemax="100" :style="getBarStyle('preprosto')">
+                <span class="sr-only">{{ results.preprosto.toFixed(2) }}%</span>
+                <div class="progress_number">
+                  {{ results.preprosto.toFixed(2).replace('.', ',') }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="me_poslanec">
+            <div class="progress smallbar">
+              <div class="progress-bar funblue" role="progressbar" :aria-valuenow="results.problematicno" aria-valuemin="0" aria-valuemax="100" :style="getBarStyle('problematicno')">
+                <span class="sr-only">{{ results.problematicno.toFixed(2) }}%</span>
+                <div class="progress_number">
+                  {{ results.problematicno.toFixed(2).replace('.', ',') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </card-wrapper>
+</template>
+
+<script>
+import common from 'mixins/common';
+import PersonPin from 'components/PersonPin.vue';
+import PartyPin from 'components/PartyPin.vue';
+
+export default {
+  name: 'ScoreAvgMax',
+  mixins: [common],
+  components: {
+    PersonPin,
+    PartyPin,
+  },
+  props: {
+    cardData: {
+      type: Object,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: value => ['poslanec', 'poslanska_skupina'].indexOf(value) > -1,
+    },
+    results: {
+      type: Object,
+      required: true,
+    },
+    person: Object,
+    party: Object,
+  },
+  methods: {
+    getBarStyle(key) {
+      return { width: `${(this.results[key] / this.maxValue) * 70}%` };
+    },
+  },
+  computed: {
+    headerConfig() {
+      let specifics;
+      if (this.type === 'poslanec') {
+        specifics = {
+          heading: this.person.name,
+          subheading: `${this.person.party.acronym} | ${this.person.party.is_coalition ? 'koalicija' : 'opozicija'}`,
+          circleImage: this.person.gov_id,
+        };
+      } else {
+        specifics = {
+          heading: this.party.name,
+          subheading: `${this.party.acronym} | ${this.party.is_coalition ? 'koalicija' : 'opozicija'}`,
+          circleText: this.party.acronym,
+          circleClass: `${this.party.acronym.replace(/ /g, '_').toLowerCase()}-background`,
+        };
+      }
+      specifics.alternative = JSON.parse(this.cardData.cardData.altHeader || 'false');
+      specifics.title = this.cardData.cardData.name;
+      return specifics;
+    },
+    cardGroup: () => this.cardData.cardData.group,
+    cardMethod: () => this.cardData.cardData.method,
+    generatedCardUrl() {
+      if (this.type === 'poslanec') {
+        return `${this.url}${this.person.id}?altHeader=true`;
+      }
+      return `${this.url}${this.party.id}?altHeader=true`;
+    },
+    maxValue() {
+      return Math.max(
+        this.results.privzdignjeno,
+        this.results.preprosto,
+        this.results.problematicno,
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.progress {
+  overflow: visible; /* this overrides bootstrap which we should get rid of anyway */
+}
+.column-title .poslanec_title {
+  line-height: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  margin-right: 10px;
+}
+.column-title .me_poslanec {
+  display: flex;
+  align-items: center;
+  height: 54px;
+}
+.me_poslanec {
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+.column-bar .progress-bar {
+  position: relative;
+}
+.column-bar .progress_number {
+  position: absolute;
+  right: 0;
+  transform: translateX(100%);
+  color: #333;
+  line-height: 30px;
+}
+</style>
---
+++ cards/p/stilne-analize/card.vue
@@ -0,0 +1,88 @@
+<template>
+  <style-scores
+    :card-data="$options.cardData"
+    :type="$options.cardData.cardData.type"
+    :person="$options.cardData.data.person"
+    :results="$options.cardData.data.results"
+  >
+    <div slot="info">
+      <p class="info-text lead">
+        Pogostosti uporabe stilno zaznamovanih besed v {{vocabulary.poslanca[gender]}} govorih.
+      </p>
+      <p class="info-text heading">METODOLOGIJA</p>
+      <p class="info-text">
+        Z združevanjem kvalifikatorjev <a class="funblue-light-hover" target="_blank" href="http://www.fran.si/130/sskj-slovar-slovenskega-knjiznega-jezika">SSKJ</a> in <a class="funblue-light-hover" target="_blank" href="http://www.fran.si/131/snb-slovar-novejsega-besedja">SNB</a> smo definirali ekscesno, privzdignjeno in preprosto besedje v poslanskih govorih.
+      </p>
+      <p class="info-text">
+        Upoštevani so le kvalifikatorji, ki so v obeh slovarjih tik za zaglavjem in imajo doseg do konca geselskega sestavka (ne pa tudi kvalifikatorji za posamezne pomene oziroma rabe).
+      </p>
+      <p class="info-text">
+        Podjetje <a class="funblue-light-hover" target="_blank" href="http://www.amebis.si/">Amebis</a> je iz transkriptov poslanskih govorov pripravilo sezname besed, ki imajo katerega od izbranih kvalifikatorjev.
+      </p>
+      <div class="info-text">
+        Ekscesno besedje:
+        <ul>
+          <li>
+            Kvalifikatorji iz SSKJ
+            <ul>
+              <li>vulgarno (vulg.), slabšalno (slabš.), ekspresivno (ekspr.), ironično (iron.), otroško (otr.)</li>
+            </ul>
+          </li>
+          <li>
+            Kvalifikatorji iz SNB
+            <ul>
+              <li>slabšalno (slabš.), vulgarno (vulg.)</li>
+            </ul>
+          </li>
+        </ul>
+
+        Privzdignjeno besedje
+        <ul>
+          <li>
+            Kvalifikatorji iz SSKJ
+            <ul>
+              <li>evfemistično (evfem.), mitološko (mitol.), pesniško (pesn.), knjižno (knjiž.), zastarelo (zastar.), starinsko (star.)</li>
+            </ul>
+          </li>
+        </ul>
+
+        Preprosto besedje
+        <ul>
+          <li>
+            Kvalifikatorji iz SSKJ
+            <ul>
+              <li>narečno (nar.), pogovorno (pog.), nižje pogovorno (nižje pog.)</li>
+            </ul>
+          </li>
+          <li>
+            Kvalifikatorji iz SNB
+            <ul>
+              <li>pogovorno (pog.)</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <p class="info-text">
+        Črtni graf prikazuje delež posameznega tipa besedja v {{vocabulary.poslanca[gender]}} govorih glede na povprečje, izračunano za celoten sklic. Delež se izračuna po naslednjem algoritmu: vzamejo se vsi {{vocabulary.poslanca3[gender]}} govori, prešteje se število besed (in njihovih ponovitev) v vsaki kategoriji besedja. To se izvede za vse poslance, nato se deli vrednost kategorije za {{vocabulary.poslanec2[gender]}} z vrednostjo povprečja (seštevek vrednosti vseh poslancev, deljen s številom poslancev). Vrednost vsake kategorije za {{vocabulary.poslanec2[gender]}} se normalizira (min. 1 – max. 100).
+      </p>
+    </div>
+  </style-scores>
+</template>
+
+<script>
+import common from 'mixins/common';
+import StyleScores from 'components/StyleScores.vue';
+
+export default {
+  name: 'StilneAnalize',
+  components: {
+    StyleScores,
+  },
+  mixins: [common],
+  data() {
+    return {
+      gender: this.$options.cardData.data.person.gender,
+    };
+  },
+};
+</script>
