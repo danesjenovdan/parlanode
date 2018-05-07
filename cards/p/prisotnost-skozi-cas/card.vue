48ac298ad8d8a0ebabe0d0f72cda15e60f27b905
LepkoQQ <lepko.san@gmail.com>
2018-05-07 18:42:25 +0200

    fix prisotnost skozi cas generated url

--- cards/p/prisotnost-skozi-cas/card.vue
+++ cards/p/prisotnost-skozi-cas/card.vue
@@ -4,19 +4,27 @@
     type="poslanec"
     :person="$options.cardData.data.person"
     :results="$options.cardData.data.results"
-    :info-text="infoText"
     v-bind="{ generatedCardUrl }"
-  />
+  >
+    <div slot="info">
+      <p class="info-text lead">
+        Prisotnost {{vocabulary.poslanec3[gender]}} na glasovanjih DZ skozi čas.
+      </p>
+      <p class="info-text heading">METODOLOGIJA</p>
+      <p class="info-text">
+        Vsak mesec preštejemo vse oddane glasovnice {{vocabulary.poslanec3[gender]}} in število delimo s številom vseh glasovanj, na katerih bi {{vocabulary.morati[gender]}} {{vocabulary.poslanec[gender]}} ta mesec glasovati. Če {{vocabulary.poslanec[gender]}} v izbranem mesecu mandata ni {{vocabulary.imeti[gender]}}, prikažemo, na kolikšnem odstotku glasovanj ni {{vocabulary.imeti[gender]}} pravice glasovati.
+      </p>
+    </div>
+  </prisotnost-chart>
 </template>
 
 <script>
 import urlFunctionalities from 'mixins/urlFunctionalities';
 import PrisotnostChart from 'components/PrisotnostChart.vue';
-
 import vocabulary from '../../../assets/vocab.json';
 
 export default {
-  name: 'PrisotnostNaSejahDz',
+  name: 'PrisotnostSkoziCas',
   components: {
     PrisotnostChart,
   },
@@ -24,11 +32,9 @@ export default {
     urlFunctionalities,
   ],
   data() {
-    const infoText = `<p class="info-text lead">Prisotnost ${vocabulary.poslanec3[this.$options.cardData.data.person.gender]} na glasovanjih DZ skozi čas.</p>
-                      <p class="info-text heading">METODOLOGIJA</p>
-                      <p class="info-text">Vsak mesec preštejemo vse oddane glasovnice ${vocabulary.poslanec3[this.$options.cardData.data.person.gender]} in število delimo s številom vseh glasovanj, na katerih bi ${vocabulary['morati'][this.$options.cardData.data.person.gender]} ${vocabulary['poslanec'][this.$options.cardData.data.person.gender]} ta mesec glasovati. Če ${vocabulary['poslanec'][this.$options.cardData.data.person.gender]} v izbranem mesecu mandata ni ${vocabulary['imeti'][this.$options.cardData.data.person.gender]}, prikažemo, na kolikšnem odstotku glasovanj ni ${vocabulary['imeti'][this.$options.cardData.data.person.gender]} pravice glasovati.</p>`;
     return {
-      infoText,
+      gender: this.$options.cardData.data.person.gender,
+      vocabulary,
       urlParameters: {},
     };
   },
