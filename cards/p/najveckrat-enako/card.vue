d0bf81b0762e6c231f24b7d660de47da23e0fa0c
LepkoQQ <lepko.san@gmail.com>
2018-05-08 17:46:00 +0200

    add "p/najveckrat-enako" and "p/najmanjkrat-enako"

---
+++ cards/p/najveckrat-enako/card.vue
@@ -0,0 +1,71 @@
+<template>
+  <card-wrapper
+    contentHeight="518px"
+    :id="$options.cardData.cardData._id"
+    :card-url="generatedCardUrl"
+    :header-config="headerConfig"
+  >
+    <div slot="info">
+      <p class="info-text lead">
+        Seznam 5 poslancev, ki največkrat glasujejo enako kot {{vocabulary.izbrani[data.person.gender]}} {{vocabulary.poslanec[data.person.gender]}}. Poslanci so razvrščeni glede na vrednosti od najmanjše proti največji. Manjša kot je vrednost, večje je ujemanje glasovanj.
+      </p>
+      <p class="info-text heading">METODOLOGIJA</p>
+      <p class="info-text">
+        Izračunamo <a href="https://en.wikipedia.org/wiki/Euclidean_distance">evklidsko razdaljo</a> med rezultati {{vocabulary.poslanca[data.person.gender]}} glasovanj in rezultati vseh ostalih (pri čemer vrednosti glasov pretvorimo v številčne vrednosti med -1 in 1).
+      </p>
+      <p class="info-text">
+        Ko izračunamo "razdaljo" med vsemi poslanci, jih razvrstimo glede na rezultat in prikažemo prvih pet.
+      </p>
+      <div class="info-text">
+        Številčne vrednosti glasov
+        <ul class="info-text">
+          <li>-1: proti</li>
+          <li>0: vzdržan/-a ali ni prisoten/-na</li>
+          <li>1: za</li>
+        </ul>
+      </div>
+    </div>
+    <person-list :people="people" :show-party-link="true" />
+  </card-wrapper>
+</template>
+
+<script>
+import common from 'mixins/common';
+import { memberOverview } from 'mixins/contextUrls';
+import { memberTitle } from 'mixins/titles';
+import PersonList from 'components/PersonList.vue';
+
+export default {
+  components: {
+    PersonList,
+    memberOverview,
+    memberTitle,
+  },
+  mixins: [common],
+  name: 'NajveckratGlasujejoEnako',
+  data() {
+    const people = this.$options.cardData.data.results.map((o) => {
+      const { person } = o;
+      person.score = `${o.ratio.toFixed(2).replace('.', ',')}`;
+      return person;
+    });
+    const { person } = this.$options.cardData.data;
+    return {
+      data: this.$options.cardData.data,
+      people,
+      headerConfig: {
+        alternative: this.$options.cardData.cardData.altHeader === 'true',
+        title: this.$options.cardData.cardData.name,
+        heading: person.name,
+        subheading: `${person.party.acronym} | ${person.party.is_coalition ? 'koalicija' : 'opozicija'}`,
+        circleImage: person.gov_id,
+      },
+    };
+  },
+  computed: {
+    generatedCardUrl() {
+      return `${this.url}${this.data.person.id}?altHeader=true`;
+    },
+  },
+};
+</script>
