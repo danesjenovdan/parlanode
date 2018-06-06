9cd7997ecb8ec3010eb3d4f13d5eddee9a3ace47
LepkoQQ <lepko.san@gmail.com>
2018-06-06 17:32:32 +0200

    i18n p/najmanjkrat-enako

--- cards/p/najmanjkrat-enako/card.vue
+++ cards/p/najmanjkrat-enako/card.vue
@@ -6,24 +6,24 @@
     :header-config="headerConfig"
   >
     <div slot="info">
-      <p class="info-text lead">
-        Seznam 5 poslancev, ki najmanjkrat glasujejo enako kot {{vocabulary.izbrani[data.person.gender]}} {{vocabulary.poslanec[data.person.gender]}}. Poslanci so razvrščeni glede na vrednosti od največje proti najmanjši. Večja kot je vrednost, manjše je ujemanje glasovanj.
-      </p>
-      <p class="info-text heading">
-        METODOLOGIJA
-      </p>
-      <p class="info-text">
-        Izračunamo <a href="https://en.wikipedia.org/wiki/Euclidean_distance">evklidsko razdaljo</a> med rezultati {{vocabulary.poslanca[data.person.gender]}} glasovanj in rezultati vseh ostalih (pri čemer vrednosti glasov pretvorimo v številčne vrednosti med -1 in 1).
-      </p>
-      <p class="info-text">
-        Ko izračunamo "razdaljo" med vsemi poslanci, jih razvrstimo glede na rezultat in prikažemo zadnjih oz. najbolj oddaljenih pet.
-      </p>
+      <p class="info-text lead" v-t="'info.lead'"></p>
+      <p class="info-text heading" v-t="'info.methodology'"></p>
+      <i18n path="info.text[0]" tag="p" class="info-text">
+        <a
+          place="link"
+          class="funblue-light-hover"
+          target="_blank"
+          :href="$t('info.link.link')"
+          v-t="'info.link.text'"
+        />
+      </i18n>
+      <p class="info-text" v-t="'info.text[1]'"></p>
       <div class="info-text">
-        Številčne vrednosti glasov
-        <ul class="info-text">
-          <li>-1: proti</li>
-          <li>0: vzdržan/-a ali ni prisoten/-na</li>
-          <li>1: za</li>
+        <span v-t="'info.text[2]'"></span>
+        <ul>
+          <li v-t="'info.list[0]'"></li>
+          <li v-t="'info.list[1]'"></li>
+          <li v-t="'info.list[2]'"></li>
         </ul>
       </div>
     </div>
@@ -35,6 +35,7 @@
 import common from 'mixins/common';
 import { memberOverview } from 'mixins/contextUrls';
 import { memberTitle } from 'mixins/titles';
+import { memberHeader } from 'mixins/altHeaders';
 import PersonList from 'components/PersonList.vue';
 
 export default {
@@ -43,7 +44,10 @@ export default {
     memberOverview,
     memberTitle,
   },
-  mixins: [common],
+  mixins: [
+    common,
+    memberHeader,
+  ],
   name: 'NajmanjkratGlasujejoEnako',
   data() {
     const people = this.$options.cardData.data.results.map((o) => {
@@ -51,17 +55,9 @@ export default {
       person.score = `${o.ratio.toFixed(2).replace('.', ',')}`;
       return person;
     });
-    const { person } = this.$options.cardData.data;
     return {
       data: this.$options.cardData.data,
       people,
-      headerConfig: {
-        alternative: this.$options.cardData.cardData.altHeader === 'true',
-        title: this.$options.cardData.cardData.name,
-        heading: person.name,
-        subheading: `${person.party.acronym} | ${person.party.is_coalition ? 'koalicija' : 'opozicija'}`,
-        circleImage: person.gov_id,
-      },
     };
   },
   computed: {
