3cd4115fcc8830e531dd780667030f71aebf6347
LepkoQQ <lepko.san@gmail.com>
2018-06-13 12:35:37 +0200

    i18n p/stevilo-izgovorjenih-besed

--- cards/p/stevilo-izgovorjenih-besed/card.vue
+++ cards/p/stevilo-izgovorjenih-besed/card.vue
@@ -6,13 +6,17 @@
     :results="$options.cardData.data.results"
   >
     <div slot="info">
-      <p class="info-text lead">
-        Število besed, ki jih je {{vocabulary.poslanec[gender]}} {{vocabulary.izgovoriti[gender]}} v vseh svojih dosedanjih govorih v DZ v primerjavi s povprečno in največjo vrednostjo.
-      </p>
-      <p class="info-text heading">METODOLOGIJA</p>
-      <p class="info-text">
-        Seštejemo besede v govorih {{vocabulary.poslanca[gender]}}, ki jih najdemo v transkriptih, pridobljenih s spletnega mesta <a href="https://www.dz-rs.si/wps/portal/Home/deloDZ/seje/sejeDrzavnegaZbora/PoDatumuSeje" target="_blank" class="funblue-light-hover">DZ RS</a>.
-      </p>
+      <p class="info-text lead" v-t="'info.lead'"></p>
+      <p class="info-text heading" v-t="'info.methodology'"></p>
+      <i18n path="info.text" tag="p" class="info-text">
+        <a
+          place="link"
+          class="funblue-light-hover"
+          target="_blank"
+          :href="$t('info.link.link')"
+          v-t="'info.link.text'"
+        />
+      </i18n>
     </div>
   </score-avg-max>
 </template>
@@ -27,10 +31,5 @@ export default {
     ScoreAvgMax,
   },
   mixins: [common],
-  data() {
-    return {
-      gender: this.$options.cardData.data.person.gender,
-    };
-  },
 };
 </script>
