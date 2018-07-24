78d24239c1ca7f906b495a8085a8373c2e6d95cc
LepkoQQ <lepko.san@gmail.com>
2018-07-24 16:13:03 +0200

    fix if govori gets empty dataset

--- cards/p/govori/card.vue
+++ cards/p/govori/card.vue
@@ -1,18 +1,51 @@
 <template>
   <govori
     :card-data="$options.cardData"
-    :person="$options.cardData.data.highlighting[0].person"
+    :person="person"
     type="person"
   />
 </template>
 
 <script>
+import { get } from 'lodash';
 import Govori from 'components/Govori.vue';
 
 export default {
   name: 'GovoriPoslanca',
   components: {
     Govori,
+  },
+  computed: {
+    person() {
+      let person = get(this.$options.cardData.data, 'highlighting[0].person');
+      if (person) {
+        return person;
+      }
+      const people = get(this.$options.cardData.data, 'facet_counts.facet_fields.speaker_i');
+      const personId = get(this.$options.cardData.data, 'filters.people[0]');
+      if (people) {
+        person = people.map(p => p.party).find(p => p.id === Number(personId));
+      }
+      if (person) {
+        return person;
+      }
+      return {
+        name: '???',
+        district: null,
+        gender: null,
+        is_active: null,
+        party: {
+          acronym: '???',
+          is_coalition: true,
+          id: 0,
+          name: '???',
+        },
+        type: '???',
+        id: 0,
+        gov_id: 'null',
+        has_function: false,
+      };
+    },
   },
 };
 </script>
