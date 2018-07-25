5e8bba2eb3230a35b129142e79a08d0b8f6912e9
LepkoQQ <lepko.san@gmail.com>
2018-07-25 16:41:38 +0200

    add previous versions button

---
+++ cards/_components/Card/Previous.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card-content-share">
+    <div class="card-back-content">
+      <ul>
+        <li v-for="prev in previous" :key="prev.url">
+          <a :href="prev.url" target="_blank" class="funblue-light-hover">{{ prev.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPrevious',
+  data() {
+    const cardData = this.$root.$options.cardData;
+    return {
+      previous: (cardData.data && cardData.data.previous_versions) || [],
+    };
+  },
+};
+</script>
