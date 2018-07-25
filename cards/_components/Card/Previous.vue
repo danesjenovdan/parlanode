ae1137f1029c30e782a29184ad0edc1aaa6c8ac5
LepkoQQ <lepko.san@gmail.com>
2018-07-25 16:40:17 +0200

    guard against undefined previous data

--- cards/_components/Card/Previous.vue
+++ cards/_components/Card/Previous.vue
@@ -2,7 +2,7 @@
   <div class="card-content-share">
     <div class="card-back-content">
       <ul>
-        <li v-for="prev in this.$root.$options.cardData.data.previous_versions" :key="prev.url">
+        <li v-for="prev in previous" :key="prev.url">
           <a :href="prev.url" target="_blank" class="funblue-light-hover">{{ prev.title }}</a>
         </li>
       </ul>
@@ -13,5 +13,11 @@
 <script>
 export default {
   name: 'CardPrevious',
+  data() {
+    const cardData = this.$root.$options.cardData;
+    return {
+      previous: (cardData.data && cardData.data.previous_versions) || [],
+    };
+  },
 };
 </script>
