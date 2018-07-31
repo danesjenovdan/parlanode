dcf9098a8cd602c0e87767e660a35beb028f66bd
LepkoQQ <lepko.san@gmail.com>
2018-07-31 11:40:57 +0200

    refactor and move configs to config folder

---
+++ parlasite/config/index.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+
+let config;
+const env = process.env.NODE_ENV;
+
+const defaultConfig = {
+  port: 3066,
+  serverTimeout: 30000,
+  urls: {
+    analize: 'https://analize.parlameter.si/v1',
+    // data: 'https://data.parlameter.si/v1',
+    // isci: 'https://isci.parlameter.si',
+    // glej: 'https://glej.parlameter.si',
+    // base: 'https://parlameter.si',
+  },
+  siteLang: 'sl',
+};
+
+if (env === 'production') {
+  // eslint-disable-next-line global-require, import/no-unresolved
+  config = require('./production');
+} else if (env === 'development') {
+  // eslint-disable-next-line global-require, import/no-unresolved
+  config = require('./development');
+} else {
+  // eslint-disable-next-line global-require
+  config = require('./sample');
+}
+
+module.exports = _.merge({}, defaultConfig, config);
---
+++ parlasite/config/production_hr.js
@@ -0,0 +1,11 @@
+module.exports = {
+  port: 7005,
+  urls: {
+    analize: 'http://analize.hr.parlameter.si/v1',
+    data: 'http://data.hr.parlameter.si/v1',
+    isci: 'http://isci.hr.parlameter.si',
+    glej: 'http://glej.hr.parlameter.si',
+    base: 'http://hr.parlameter.si',
+  },
+  siteLang: 'hr',
+};
