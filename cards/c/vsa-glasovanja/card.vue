aa09f2b5ddce2ad45959419cefe4f2d7878d1066
LepkoQQ <lepko.san@gmail.com>
2018-09-12 12:37:30 +0200

    add info text to c/vsa-glasovanja

--- cards/c/vsa-glasovanja/card.vue
+++ cards/c/vsa-glasovanja/card.vue
@@ -5,6 +5,48 @@
     :header-config="headerConfig"
     :og-config="ogConfig"
   >
+    <div slot="info">
+      <p v-t="'info.lead'" class="info-text lead"></p>
+      <p v-t="'info.methodology'" class="info-text heading"></p>
+      <p v-t="'info.text[0]'" class="info-text"></p>
+      <i18n path="info.text[1]" tag="p" class="info-text">
+        <a
+          v-t="'info.links[0].text'"
+          :href="$t('info.links[0].link')"
+          place="link"
+          class="funblue-light-hover"
+          target="_blank"
+        />
+      </i18n>
+      <i18n path="info.text[2]" tag="p" class="info-text">
+        <a
+          v-t="'info.links[1].text'"
+          :href="$t('info.links[1].link')"
+          place="link1"
+          class="funblue-light-hover"
+          target="_blank"
+        />
+        <a
+          v-t="'info.links[2].text'"
+          :href="$t('info.links[2].link')"
+          place="link2"
+          class="funblue-light-hover"
+          target="_blank"
+        />
+      </i18n>
+      <i18n path="info.text[3]" tag="p" class="info-text">
+        <a
+          v-t="'info.links[3].text'"
+          :href="$t('info.links[3].link')"
+          place="link"
+          class="funblue-light-hover"
+          target="_blank"
+        />
+      </i18n>
+      <p v-t="'info.text[4]'" class="info-text"></p>
+
+    </div>
+
     <seznam-glasovanj
       :data="votes"
       :filters="filters"
@@ -19,7 +61,6 @@ import common from 'mixins/common';
 import links from 'mixins/links';
 import { defaultHeaderConfig } from 'mixins/altHeaders';
 import { defaultOgImage } from 'mixins/ogImages';
-// import { otherVotingsTitle } from 'mixins/titles';
 import SeznamGlasovanj from 'components/SeznamGlasovanj.vue';
 
 export default {
@@ -29,7 +70,6 @@ export default {
   },
   mixins: [
     common,
-    // otherVotingsTitle,
     links,
   ],
   data() {
