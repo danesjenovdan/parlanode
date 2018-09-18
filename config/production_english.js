1faf056ae325428b1530608d100036e9988ffc29
LepkoQQ <lepko.san@gmail.com>
2018-09-18 18:20:28 +0200

    add config from parlaenglish

---
+++ config/production_english.js
@@ -0,0 +1,15 @@
+module.exports = {
+  port: 7004,
+  db: {
+    name: 'parladb',
+  },
+  urls: {
+    cdn: 'http://cdn.english.parlameter.si/v1/parlassets',
+    analize: 'https://analize.parlameter.si/v1',
+    data: 'https://data.parlameter.si/v1',
+    isci: 'https://isci.parlameter.si',
+    glej: 'http://glej.english.parlameter.si',
+    base: 'http://english.parlameter.si',
+  },
+  cardLang: 'en',
+};
