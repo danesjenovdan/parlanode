855fd7a9357d104389b7c62757bb468c3319f32a
LepkoQQ <lepko.san@gmail.com>
2018-09-19 17:30:16 +0200

    add refetch api endpoint

--- parlasite/server/routes/api.js
+++ parlasite/server/routes/api.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const data = require('../data');
 const config = require('../../config');
 
 const router = express.Router();
 
 router.get('/sitemap', (req, res) => {
   res.json(config.siteMap);
+});
+
+router.get('/data/refetch', (req, res) => {
+  Promise.resolve()
+    .then(() => data.refetch())
+    .then((error) => {
+      if (!error) {
+        res.json({
+          ok: true,
+          message: 'Finished refetch',
+        });
+      } else {
+        res.json({
+          ok: false,
+          message: 'Failed refetch',
+          error,
+        });
+      }
+    });
 });
 
 module.exports = router;
