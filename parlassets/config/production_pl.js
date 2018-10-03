5d4ecaf1e3309bb68570ed3f7a022e8f5dfac125
LepkoQQ <lepko.san@gmail.com>
2018-10-03 15:45:58 +0200

    add pl config

---
+++ parlassets/config/production_pl.js
@@ -0,0 +1,10 @@
+module.exports = {
+  urls: {
+    cdn: 'http://cdn.pl.parlameter.si/v1/parlassets',
+    analize: 'http://analize.pl.parlameter.si/v1',
+    data: 'http://data.pl.parlameter.si/v1',
+    isci: 'http://isci.pl.parlameter.si',
+    glej: 'http://glej.pl.parlameter.si',
+    base: 'http://pl.parlameter.si',
+  },
+};
