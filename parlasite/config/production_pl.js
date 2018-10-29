07295e9014eecc9cb443238fc5777ea6d50cb9d9
LepkoQQ <lepko.san@gmail.com>
2018-10-29 12:57:18 +0100

    update pl domain in config

--- parlasite/config/production_pl.js
+++ parlasite/config/production_pl.js
@@ -1,12 +1,12 @@
 module.exports = {
   port: 7005,
   urls: {
-    cdn: 'http://cdn.pl.parlameter.si/v1/parlassets',
-    analize: 'http://analize.pl.parlameter.si/v1',
-    data: 'http://data.pl.parlameter.si/v1',
-    isci: 'http://isci.pl.parlameter.si',
-    glej: 'http://glej.pl.parlameter.si',
-    base: 'http://pl.parlameter.si',
+    cdn: 'https://cdn.parlametr.pl/v1/parlassets',
+    analize: 'https://analize.parlametr.pl/v1',
+    data: 'https://data.parlametr.pl/v1',
+    isci: 'https://isci.parlametr.pl',
+    glej: 'https://glej.parlametr.pl',
+    base: 'https://parlametr.pl',
   },
   siteLang: 'pl',
 };
