a8fafdc7ce468f58a26e650b11c64633e8eb7c82
LepkoQQ <lepko.san@gmail.com>
2018-12-18 17:42:55 +0100

    add urls to dev html and add link to refetch dev data

--- cards/webpack.config.dev.js
+++ cards/webpack.config.dev.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
+const fs = require('fs-extra');
 const path = require('path');
 const merge = require('webpack-merge');
+const axios = require('axios');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const baseConfig = require('./webpack.config.base');
 
@@ -35,6 +37,21 @@ module.exports = merge.smart(baseConfig(process.env.CARD_NAME), {
     stats: 'minimal',
     watchOptions: {
       aggregateTimeout: 1000,
+    },
+    before(app) {
+      app.get('/dev-api/refetch-data', (req, res) => {
+        axios.get(req.query.url)
+          .then((resp) => {
+            const dataFilePath = path.resolve(__dirname, process.env.CARD_NAME, 'data.json');
+            if (fs.existsSync(dataFilePath)) {
+              fs.writeJsonSync(dataFilePath, resp.data, { spaces: 2 });
+            }
+            res.json(resp.data);
+          })
+          .catch((error) => {
+            res.send(error.message);
+          });
+      });
     },
   },
   plugins: [
