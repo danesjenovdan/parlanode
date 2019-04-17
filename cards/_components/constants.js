7903990a1b9dba3f1580500ba4eb7d767dd2e46a
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-04-17 15:25:19 +0200

    add constant for PG classifications and generators for peopla and PGs

--- cards/_components/constants.js
+++ cards/_components/constants.js
@@ -1,3 +1,4 @@
 export const MONTH_NAMES = ['Januar', 'Februar', 'Marec', 'April', 'Maj', 'Junij', 'Julij', 'Avgust', 'September', 'Oktober', 'November', 'December'];
 export const RIPPLE_DURATION = 600;
 export const SPEECHES_PER_PAGE = 150;
+export const PG_CLASSIFICATIONS = ['pg', 'club'];
