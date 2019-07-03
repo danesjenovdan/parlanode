8f527fcdc269a3da21b0b00b11168a263db1fd32
LepkoQQ <lepko.san@gmail.com>
2019-07-03 17:34:17 +0200

    add analizeV2 url to config

--- parlasite/config/production_hr.js
+++ parlasite/config/production_hr.js
@@ -3,6 +3,7 @@ module.exports = {
   urls: {
     cdn: 'https://cdn.parlametar.hr/v1/parlassets',
     analize: 'https://analize.parlametar.hr/v1',
+    analizeV2: 'https://analize.parlametar.hr/v2',
     data: 'https://data.parlametar.hr/v1',
     isci: 'https://isci.parlametar.hr',
     glej: 'https://glej.parlametar.hr',
