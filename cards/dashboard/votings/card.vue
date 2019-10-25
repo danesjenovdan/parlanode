4fa715a3315557252b5abfe3650f6fca295b7051
LepkoQQ <lepko.san@gmail.com>
2019-10-25 12:54:11 +0200

    allow sorting dash votings by name or date

--- cards/dashboard/votings/card.vue
+++ cards/dashboard/votings/card.vue
@@ -4,11 +4,22 @@
       <div id="dash-votings-list">
         <h4 v-if="session">{{ session.name }}</h4>
         <div v-if="votings != null" class="filters">
-          <input
-            v-model="filterQuery"
-            :placeholder="$t('search')"
-            class="form-control"
-          >
+          <div class="filter-input">
+            <input
+              v-model="filterQuery"
+              :placeholder="$t('search')"
+              class="form-control"
+            >
+          </div>
+          <div class="filter-sort">
+            <p-search-dropdown
+              v-model="sortOptions"
+              single
+              small
+              hide-clear
+              @select="sortBy = $event"
+            />
+          </div>
         </div>
         <dash-table
           :items="mappedItems"
@@ -149,13 +160,30 @@ export default {
       tagModalData: null,
       error: null,
       filterQuery: '',
+      sortBy: 'start_time',
+      sortOptions: [
+        {
+          id: 'start_time',
+          label: 'start_time',
+          selected: true,
+        },
+        {
+          id: 'name',
+          label: this.$t('name'),
+          selected: false,
+        },
+      ],
     };
   },
   computed: {
     mappedItems() {
       if (this.votings) {
         const q = this.filterQuery.toLowerCase();
-        return this.votings
+        const sortedVotings = orderBy(this.votings, (o) => {
+          const value = o[this.sortBy];
+          return value.toLowerCase ? value.toLowerCase() : value;
+        }, ['desc']);
+        return sortedVotings
           .filter(v => v.name.toLowerCase().indexOf(q) !== -1)
           .map((voting) => {
             const motion = this.motions.find(m => m.id === voting.motion);
@@ -194,7 +222,7 @@ export default {
       this.$parlapi.getSession(this.sessionId),
     ])
       .then(([votings, motions, tags, sessions]) => {
-        this.votings = orderBy(votings.data.results, ['start_time'], ['desc']);
+        this.votings = votings.data.results;
         this.motions = motions.data.results;
         this.tags = tags.data.results;
         this.session = sessions.data.results.length ? sessions.data.results[0] : {};
@@ -206,6 +234,9 @@ export default {
       });
   },
   methods: {
+    changeSort(value) {
+      this.sortBy = value;
+    },
     changeResult(motionId, value) {
       const motion = this.motions.find(m => m.id === motionId);
       if (motion) {
@@ -293,6 +324,24 @@ export default {
 .card-container,
 /deep/ .card-content {
   overflow: visible;
+}
+
+.filters {
+  display: flex;
+
+  .filter-input {
+    flex: 3;
+    margin-right: 10px;
+  }
+
+  .filter-sort {
+    flex: 1;
+
+    /deep/ .search-dropdown-input {
+      line-height: 22px;
+      font-size: 14px;
+    }
+  }
 }
 
 #dash-votings-list /deep/ {
