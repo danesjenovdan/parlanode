0a957d50cf4f3d949f3b4cae357ee2a591a73623
LepkoQQ <lepko.san@gmail.com>
2021-04-19 21:58:38 +0200

    remove old server

--- server_old/resources/cards/routes.js
+++
@@ -1,8 +0,0 @@
-const controller = require('./controller');
-
-module.exports = (app) => {
-  app.get('/:group/:method/', controller.render);
-  app.get('/:group/:method/:id(\\d+)/', controller.render);
-  app.get('/:group/:method/:id(\\d+)/:date(\\d{1,2}.\\d{1,2}.\\d{4})/', controller.render);
-  app.get('/:group/:method/:date(\\d{1,2}.\\d{1,2}.\\d{4})/', controller.render);
-};
--- server_old/resources/og-images/routes.js
+++
@@ -1,5 +0,0 @@
-const controller = require('./controller');
-
-module.exports = (app) => {
-  app.get('/og-image/:name/', controller.render);
-};
