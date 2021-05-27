46b54cc9cd932a237833ff04879b07ed76c3ab5b
LepkoQQ <lepko.san@gmail.com>
2021-05-27 17:06:21 +0200

    add back stateLoader.js removed by mistake

---
+++ cards/_helpers/stateLoader.js
@@ -0,0 +1,22 @@
+export default (state) => {
+  if (!state) {
+    return () => false;
+  }
+
+  return (key, fullOptions) => {
+    if (!Object.prototype.hasOwnProperty.call(state, key)) {
+      return false;
+    }
+
+    if (fullOptions) {
+      // Full options have been passed, so the value in the state should be
+      // an array of IDs, which must be mapped, not returned directly.
+      return fullOptions.map((option) => ({
+        ...option,
+        selected: state[key].indexOf(option.id) > -1,
+      }));
+    }
+
+    return state[key];
+  };
+};
