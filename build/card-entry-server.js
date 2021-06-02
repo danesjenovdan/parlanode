1dc586b8fe5238e16275e2e5bf8d801d0172d54c
LepkoQQ <lepko.san@gmail.com>
2021-06-02 10:18:06 +0200

    hacky workaround for broken cards breaking subsequent card renders

--- build/card-entry-server.js
+++ build/card-entry-server.js
@@ -1,4 +1,4 @@
-import { createSSRApp } from 'vue';
+import { createSSRApp, ssrUtils } from 'vue';
 import { createI18n } from 'vue-i18n';
 import { renderToString } from '@vue/server-renderer';
 import { merge } from 'lodash-es';
@@ -17,7 +17,27 @@ export default async (contextData, i18nData) => {
   const app = createSSRApp({ ...Card, contextData });
   app.use(i18n);
 
-  const ctx = {};
-  const html = await renderToString(app, ctx);
-  return [html, ctx];
+  try {
+    const ctx = {};
+    const html = await renderToString(app, ctx);
+    return [html, ctx];
+  } finally {
+    //
+    // THIS I A HACKY WORKAROUND: There is a bug in vue ssr renderer!
+    //
+    // NOTE (LepkoQQ): I tried to reproduce and report it, but I can't seem to
+    // reproduce it in a simplified test case and they don't accept bug reports
+    // without a minimal repro.
+    //
+    // If a component errors in data or a computed property that is immediately
+    // accessed, the error is sometimes not caught, and cleanup code is not run.
+    //
+    // The next component that will want to resolve a component name will think
+    // its running in the context that was left over from the errored component
+    // and fail to resolve it.
+    //
+    // This should always be set to undefined after every render.
+    //
+    ssrUtils.setCurrentRenderingInstance(undefined);
+  }
 };
