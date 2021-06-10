4c72259bf3d001fe83448a1d38578786a0c241be
LepkoQQ <lepko.san@gmail.com>
2021-06-10 15:34:10 +0200

    update group/questions for ljubljana

--- cards/group/questions/card.vue
+++ cards/group/questions/card.vue
@@ -1,20 +1,25 @@
 <template>
-  <poslanska-vprasanja-in-pobude
-    :context-data="$options.contextData"
-    type="party"
-  />
+  <card-wrapper :header-config="headerConfig" :og-config="ogConfig">
+    <question-list :question-days="cardData.data?.results" show-author />
+  </card-wrapper>
 </template>
 
 <script>
-import PoslanskaVprasanjaInPobude from '@/_components/PoslanskaVprasanjaInPobude.vue';
+import common from '@/_mixins/common.js';
+import { partyOverview } from '@/_mixins/contextUrls.js';
+import { partyTitle } from '@/_mixins/titles.js';
+import { partyHeader } from '@/_mixins/altHeaders.js';
+import { partyOgImage } from '@/_mixins/ogImages.js';
+import QuestionList from '@/_components/QuestionList.vue';
 
 export default {
   name: 'CardGroupQuestions',
   components: {
-    PoslanskaVprasanjaInPobude,
+    QuestionList,
   },
   cardInfo: {
     doubleWidth: true,
   },
+  mixins: [common, partyOverview, partyTitle, partyHeader, partyOgImage],
 };
 </script>
