63cadc078bff5a1f0a50b76d3573ba2198a33885
LepkoQQ <lepko.san@gmail.com>
2021-06-30 17:46:05 +0200

    fix session legislation

--- cards/session/legislation/card.vue
+++ cards/session/legislation/card.vue
@@ -243,9 +243,9 @@ export default {
               b = B.text;
               return a.toLowerCase().localeCompare(b.toLowerCase(), 'sl');
             case 'updated':
-              a = dateParser(A.date).getTime() || Date.now();
-              b = dateParser(B.date).getTime() || Date.now();
-              return a - b;
+              a = A.timestamp;
+              b = B.timestamp;
+              return a.toLowerCase().localeCompare(b.toLowerCase(), 'sl');
             case 'workingBody':
               a = A.mdt_text;
               b = B.mdt_text;
