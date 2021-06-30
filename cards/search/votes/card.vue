25a457a111488cdd30e8cac4a796e92c05716f84
LepkoQQ <lepko.san@gmail.com>
2021-06-30 17:38:18 +0200

    fix search votes

--- cards/search/votes/card.vue
+++ cards/search/votes/card.vue
@@ -1,81 +1,40 @@
 <template>
-  <card-wrapper
-    :content-class="['full', { 'is-loading': loading }]"
-    :header-config="headerConfig"
-    :og-config="ogConfig"
-  >
-    <template #info>
-      <p v-t="'info.lead'" class="info-text lead"></p>
-      <p v-t="'info.methodology'" class="info-text heading"></p>
-      <p v-t="'info.text'" class="info-text"></p>
-    </template>
-
-    <seznam-glasovanj v-if="data" :data="votes" :show-filters="false" />
+  <card-wrapper :header-config="headerConfig" :og-config="ogConfig">
+    <vote-list
+      :show-filters="false"
+      :votes-and-stuff="votes"
+      :virtualize-remain="5"
+      virtualize
+    />
   </card-wrapper>
 </template>
 
 <script>
-import axios from 'axios';
-import SeznamGlasovanj from '@/_components/SeznamGlasovanj.vue';
+import VoteList from '@/_components/VoteList.vue';
 import common from '@/_mixins/common.js';
 import { search as searchContext } from '@/_mixins/contextUrls.js';
 import { searchHeader } from '@/_mixins/altHeaders.js';
 import { searchOgImage } from '@/_mixins/ogImages.js';
 import { searchTitle } from '@/_mixins/titles.js';
-import stateLoader from '@/_helpers/stateLoader.js';
 
 export default {
   name: 'CardSearchVotes',
   components: {
-    SeznamGlasovanj,
+    VoteList,
   },
   mixins: [common, searchTitle, searchHeader, searchOgImage, searchContext],
-  data() {
-    const loadFromState = stateLoader(this.$options.cardData.parlaState);
-    return {
-      data: null,
-      currentSort: '',
-      currentSortOrder: 'DESC',
-      workingBodies: [],
-      keywords: loadFromState('query'),
-      loading: true,
-    };
+  cardInfo: {
+    doubleWidth: true,
   },
   computed: {
     votes() {
-      if (this.data) {
-        return {
-          votes: this.data.map((motion) => ({
-            ...motion.results,
-            session_id: motion.session.id,
-          })),
-          session: {},
-          tags: [],
-        };
-      }
       return {
-        votes: [],
-        session: {},
-        tags: [],
+        votes: this.cardData.data?.results,
+        session: this.cardData.data?.session,
+        tags: [], // this.data.tags,
+        classifications: [], // this.data.classifications,
       };
     },
-  },
-  mounted() {
-    const searchUrl = `${
-      this.slugs.urls.isci
-    }/search/votes?q=${encodeURIComponent(this.keywords)}`;
-    axios
-      .get(searchUrl)
-      .then((res) => {
-        this.data = (res.data.response && res.data.response.docs) || [];
-        this.loading = false;
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        this.loading = false;
-        this.error = true;
-      });
   },
 };
 </script>
