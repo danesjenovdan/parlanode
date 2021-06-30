5c4d8779b5d0db3e5876f6ea8d2d3ea5884e4761
LepkoQQ <lepko.san@gmail.com>
2021-06-30 13:31:41 +0200

    fix search/most-used-by-people

--- cards/search/most-used-by-people/card.vue
+++ cards/search/most-used-by-people/card.vue
@@ -1,33 +1,17 @@
 <template>
-  <card-wrapper
-    :content-class="['full', { 'is-loading': loading }]"
-    :header-config="headerConfig"
-    :og-config="ogConfig"
-  >
-    <template #info>
-      <p v-t="'info.lead'" class="info-text lead"></p>
-      <p v-t="'info.methodology'" class="info-text heading"></p>
-      <p v-t="'info.text'" class="info-text"></p>
-    </template>
-
-    <div
-      v-if="!loading && people && people.length === 0"
-      v-t="'no-results'"
-      class="no-results"
-    />
-    <person-list v-else :people="people" :show-party-link="true" />
+  <card-wrapper :header-config="headerConfig" :og-config="ogConfig">
+    <person-list :people="people" :show-party-link="true" />
   </card-wrapper>
 </template>
 
 <script>
-import axios from 'axios';
 import common from '@/_mixins/common.js';
 import { search as searchContext } from '@/_mixins/contextUrls.js';
 import { searchTitle } from '@/_mixins/titles.js';
 import { searchHeader } from '@/_mixins/altHeaders.js';
 import { searchOgImage } from '@/_mixins/ogImages.js';
 import PersonList from '@/_components/PersonList.vue';
-import stateLoader from '@/_helpers/stateLoader.js';
+import numberFormatter from '@/_helpers/numberFormatter.js';
 
 export default {
   name: 'CardSearchMostUsedByPeople',
@@ -36,45 +20,16 @@ export default {
   },
   mixins: [common, searchTitle, searchHeader, searchOgImage, searchContext],
   data() {
-    const loadFromState = stateLoader(this.$options.cardData.parlaState);
+    const results = (this.cardData.data?.results || [])
+      .slice()
+      .sort((a, b) => b.value - a.value);
+    const people = results.map((entry) => ({
+      ...entry.person,
+      score: numberFormatter(entry.value),
+    }));
     return {
-      currentSort: '',
-      currentSortOrder: 'DESC',
-      workingBodies: [],
-      keywords: loadFromState('query'),
-      mps: loadFromState('mps') || [],
-      pgs: loadFromState('pgs') || [],
-      loading: true,
-      people: [],
+      people,
     };
-  },
-
-  mounted() {
-    const searchUrl = `${
-      this.slugs.urls.isci
-    }/search/speeches?q=${encodeURIComponent(
-      this.keywords
-    )}&people=${this.mps.join(',')}&parties=${this.pgs.join(',')}`;
-    axios
-      .get(searchUrl)
-      .then((res) => {
-        const people = res.data.facet_counts.facet_fields.person
-          .map((o) => {
-            const { person } = o;
-            person.score = `${Math.round(o.score)}`;
-            return person;
-          })
-          .filter((person) => person.score > 0)
-          .slice(0, 5);
-        this.people = people;
-        this.loading = false;
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        this.loading = false;
-        this.error = true;
-      });
   },
 };
 </script>
