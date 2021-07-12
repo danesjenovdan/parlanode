da0c85ca3abb2eafcef4ff3ece7503963610f039
LepkoQQ <lepko.san@gmail.com>
2021-07-12 18:52:38 +0200

    fix misc/last-session

--- cards/misc/last-session/card.vue
+++ cards/misc/last-session/card.vue
@@ -1,56 +1,65 @@
 <template>
   <card-wrapper :header-config="headerConfig" :og-config="ogConfig" max-height>
-    <div class="smalldate">{{ data.session.date }}</div>
+    <div class="session-name">{{ session.name }}</div>
+    <div class="session-date">{{ formatDate(session.start_time) }}</div>
     <hr />
     <div class="link">
-      <a
-        :href="getSessionTranscriptLink(data.session)"
-        class="funblue-light-hover"
-      >
+      <a :href="getSessionTranscriptLink(session)" class="funblue-light-hover">
         <span class="glyphicon glyphicon-comment"></span>
-        <span v-t="'info.words-heading'"></span>
+        <span v-t="'card.tfidf-title'"></span>
       </a>
     </div>
     <div v-if="chartRows.length" class="columns">
-      <bar-chart :data="chartRows1" :already-calculated="true" />
-      <bar-chart :data="chartRows2" :already-calculated="true" />
+      <bar-chart :data="chartRows1" :max="max" :total="total" />
+      <bar-chart :data="chartRows2" :max="max" :total="total" />
     </div>
     <hr />
     <div class="link">
       <span class="link-color">
         <span class="glyphicon glyphicon-comment"></span>
-        <span v-t="'info.presence-heading'"></span>
+        <span v-t="'card.attendance-title'"></span>
       </span>
     </div>
-    <prisotnost-po-poslanskih-skupinah :data="data.presence" />
+    <!-- <prisotnost-po-poslanskih-skupinah :data="attendance" /> -->
     <hr />
     <div class="link">
-      <a :href="getSessionVotesLink(data.session)" class="funblue-light-hover">
+      <a :href="getSessionVotesLink(session)" class="funblue-light-hover">
         <span class="glyphicon glyphicon-comment"></span>
-        <span v-t="'info.votes-heading'"></span>
+        <span v-t="'card.votes-title'"></span>
       </a>
     </div>
-    <div class="votes">
-      <seznam-glasovanj :data="votes" :show-filters="false" />
+    <div class="votes-list">
+      <scroll-shadow ref="shadow">
+        <div
+          class="votes-list-shadow"
+          @scroll="$refs.shadow.check($event.currentTarget)"
+        >
+          <vote-list-item v-for="vote in votes" :key="vote.id" :vote="vote" />
+        </div>
+      </scroll-shadow>
     </div>
   </card-wrapper>
 </template>
 
 <script>
+import { max, sum } from 'lodash-es';
 import common from '@/_mixins/common.js';
 import { sessionHeader } from '@/_mixins/altHeaders.js';
 import { sessionOgImage } from '@/_mixins/ogImages.js';
 import BarChart from '@/_components/BarChart.vue';
-import PrisotnostPoPoslanskihSkupinah from '@/_components/PrisotnostPoPoslanskihSkupinah.vue';
-import SeznamGlasovanj from '@/_components/SeznamGlasovanj.vue';
+// import PrisotnostPoPoslanskihSkupinah from '@/_components/PrisotnostPoPoslanskihSkupinah.vue';
+import ScrollShadow from '@/_components/ScrollShadow.vue';
+import VoteListItem from '@/_components/VoteListItem.vue';
 import links from '@/_mixins/links.js';
+import dateFormatter from '@/_helpers/dateFormatter.js';
 
 export default {
   name: 'CardMiscLastSession',
   components: {
     BarChart,
-    SeznamGlasovanj,
-    PrisotnostPoPoslanskihSkupinah,
+    // PrisotnostPoPoslanskihSkupinah,
+    VoteListItem,
+    ScrollShadow,
   },
   mixins: [common, sessionHeader, sessionOgImage, links],
   cardInfo: {
@@ -58,35 +67,25 @@ export default {
   },
   data() {
     return {
-      data: this.$options.contextData.cardData,
+      session: this.cardData.data?.session || {},
+      tfidf: this.cardData.data?.results?.tfidf || [],
+      attendance: this.cardData.data?.results?.attendance || [],
+      votes: this.cardData.data?.results?.votes || [],
     };
   },
   computed: {
+    max() {
+      return max(this.tfidf.map((item) => item.value || 0)) * 5000;
+    },
+    total() {
+      return sum(this.tfidf.map((item) => item.value || 0)) * 5000;
+    },
     chartRows() {
-      // JSON.parse(JSON.stringify(this.data.results));
-      const rows = this.data.tfidf.results.map((row) => {
-        row.value = Math.round(row.scores['tf-idf'] * 5000);
-        return row;
-      });
-
-      const mymax = this.data.tfidf.results.reduce(
-        (acc, row) => Math.max(acc, row.value),
-        0
-      );
-      const mytotal = this.data.tfidf.results.reduce(
-        (acc, row) => acc + row.value,
-        0
-      );
-
-      return rows
-        .map((row) => ({
-          name: this.decodeHTML(row.term),
-          value: row.value,
-          link: this.getSearchTermLink(row.term),
-          widthPercentage: (row.value / mymax) * 100,
-          percentage: ((row.value / mytotal) * 100).toFixed(2),
-        }))
-        .sort((a, b) => b.value - a.value);
+      return this.tfidf.map((item) => ({
+        label: item.token,
+        value: Math.round(item.value * 5000),
+        link: this.getSearchTermLink(item.term),
+      }));
     },
     chartRows1() {
       return this.chartRows.slice(0, 5);
@@ -94,22 +93,10 @@ export default {
     chartRows2() {
       return this.chartRows.slice(5, 10);
     },
-    votes() {
-      return {
-        votes: this.data.motions.map((motion) => motion.results),
-        session: this.data.session,
-        tags: this.data.motions
-          .map((motion) => motion.results.tags)
-          .reduce((acc, cur) => {
-            acc.push(...cur);
-            return acc;
-          }, []),
-      };
-    },
   },
   methods: {
-    decodeHTML(html) {
-      return html.replace('&shy;', '\u00AD');
+    formatDate(date) {
+      return dateFormatter(date);
     },
   },
 };
@@ -119,11 +106,29 @@ export default {
 @import 'parlassets/scss/breakpoints';
 @import 'parlassets/scss/colors';
 
+.session-name {
+  font-weight: 500;
+  font-size: 16px;
+}
+
 hr,
 .link {
   margin-top: 10px;
   margin-bottom: 10px;
   text-transform: uppercase;
+
+  .link-color {
+    color: $link;
+  }
+
+  .glyphicon {
+    margin-right: 5px;
+
+    &::before {
+      position: relative;
+      bottom: -1px;
+    }
+  }
 }
 
 .columns {
@@ -144,7 +149,7 @@ hr,
       margin-bottom: 0;
       margin-right: 0 !important;
 
-      :deep(column-label) {
+      :deep(.column-label) {
         flex: 0 0 33%;
 
         .chart-label {
@@ -152,7 +157,7 @@ hr,
         }
       }
 
-      :deep(column-bar) {
+      :deep(.column-bar) {
         flex: 0 0 66%;
       }
     }
@@ -163,16 +168,11 @@ hr,
   }
 }
 
-.votes {
-  min-height: 40px;
-
-  :deep(votingCard) {
-    height: auto !important;
-    max-height: 500px;
+.votes-list {
+  .votes-list-shadow {
+    overflow-y: auto;
+    overflow-x: hidden;
+    height: $full-card-height;
   }
-}
-
-.link-color {
-  color: $link;
 }
 </style>
