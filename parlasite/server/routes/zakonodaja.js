c74a07438f3484a7c715f97ad804fc0322a8e418
muki <filipdobranic@gmail.com>
2021-07-14 10:24:35 +0200

    make single legislation work

--- parlasite/server/routes/zakonodaja.js
+++ parlasite/server/routes/zakonodaja.js
@@ -2,8 +2,22 @@ const express = require('express');
 const data = require('../data');
 const { asyncRender: ar } = require('../utils');
 const { i18n } = require('../server');
+const fetch = require('node-fetch');
 
 const router = express.Router();
+
+async function getNewData(slug) {
+  const id = parseInt(slug.split('-')[0]);
+  // TODO this shouldn't be hard-coded
+  const response = await fetch(`https://parladata.lb.djnd.si/v3/cards/legislation/single?id=${id}`);
+  if (response.ok && response.status >= 200 && response.status < 400) {
+    let data = await response.json();
+    return {
+      ...data,
+    };
+  }
+  return false;
+}
 
 router.get('/', ar((render) => {
   render('zakonodaja', {
@@ -12,18 +26,18 @@ router.get('/', ar((render) => {
   });
 }));
 
-router.get('/*', ar((render, req, res, next) => {
-  const epa = req.params[0];
-  const lawData = data.laws.find(law => law.epa === epa);
-  if (lawData) {
-    render('zakonodaja/zakon', {
-      activeMenu: 'legislation_act',
-      pageTitle: i18n('titles.legislation'),
-      lawData,
-    });
-  } else {
-    next();
-  }
+router.get('/:slug([a-z0-9-]+)', ar((render, req, res, next) => {
+  getNewData(req.params.slug).then((lawData) => {
+    if (lawData) {
+      render('zakonodaja/zakon', {
+        activeMenu: 'legislation_act',
+        pageTitle: i18n('titles.legislation'),
+        lawData,
+      });
+    } else {
+      next();
+    }
+  });
 }));
 
 module.exports = router;
