5372c07fcd6fc4387eaafa89c0e9f62dcb20fb47
LepkoQQ <lepko.san@gmail.com>
2021-07-26 20:55:40 +0200

    fix urls not handling slugs with only numbers

--- parlasite/server/routes/poslanec.js
+++ parlasite/server/routes/poslanec.js
@@ -1,31 +1,31 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const { asyncRender: ar } = require('../utils');
 const { siteMap: sm, urls } = require('../../config');
 const { i18n } = require('../server');
-const fetch = require('node-fetch');
 
 const router = express.Router();
 
 async function getNewData(slug) {
-  const id = parseInt(slug.split('-')[0]);
+  const id = parseInt(slug.split('-')[0], 10);
   // TODO this shouldn't be hard-coded
-  const response = await fetch(`${urls.parladata}/cards/person/basic-information?id=${id}`);
-  if (response.ok && response.status >= 200 && response.status < 400) {
-    let data = await response.json();
+  const response = await fetch(`${urls.parladata}/cards/person/basic-information/?id=${id}`);
+  // response.ok means status is 2xx
+  if (response.ok) {
+    const data = await response.json();
     return {
       mp: {
         ...data.person,
         ...data.results,
         id, // TODO this might be simpler if parladata would return the ID
-      }
+      },
     };
   }
   return false;
 }
 
-router.get(['/:id(\\d+)', `/:id(\\d+)/${sm.member.overview}`, '/:slug([a-z0-9-]+)', `/:slug([a-z0-9-]+)/${sm.member.overview}`], ar((render, req, res, next) => {
-  const mpData = getNewData(req.params.slug).then((mpData) => {
-    console.log(mpData);
+router.get(['/:slug([a-z0-9-]+)', `/:slug([a-z0-9-]+)/${sm.member.overview}`], ar((render, req, res, next) => {
+  getNewData(req.params.slug).then((mpData) => {
     if (mpData) {
       render('poslanec/pregled', {
         activeMenu: 'mp',
@@ -39,9 +39,8 @@ router.get(['/:id(\\d+)', `/:id(\\d+)/${sm.member.overview}`, '/:slug([a-z0-9-]+
   });
 }));
 
-router.get([`/:id(\\d+)/${sm.member.votings}`, `/:slug([a-z0-9-]+)/${sm.member.votings}`], ar((render, req, res, next) => {
-  const mpData = getNewData(req.params.slug).then((mpData) => {
-    console.log(mpData);
+router.get([`/:slug([a-z0-9-]+)/${sm.member.votings}`], ar((render, req, res, next) => {
+  getNewData(req.params.slug).then((mpData) => {
     if (mpData) {
       render('poslanec/glasovanja', {
         activeMenu: 'mp',
@@ -55,9 +54,8 @@ router.get([`/:id(\\d+)/${sm.member.votings}`, `/:slug([a-z0-9-]+)/${sm.member.v
   });
 }));
 
-router.get([`/:id(\\d+)/${sm.member.speeches}`, `/:slug([a-z0-9-]+)/${sm.member.speeches}`], ar((render, req, res, next) => {
-  const mpData = getNewData(req.params.slug).then((mpData) => {
-    console.log(mpData);
+router.get([`/:slug([a-z0-9-]+)/${sm.member.speeches}`], ar((render, req, res, next) => {
+  getNewData(req.params.slug).then((mpData) => {
     if (mpData) {
       render('poslanec/govori', {
         activeMenu: 'mp',
