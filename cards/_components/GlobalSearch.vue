fe43a55f091b495d9c898d68ff3eeb754a7dc727
LepkoQQ <lepko.san@gmail.com>
2021-10-14 19:51:03 +0200

    update misc/search for new dynamic api

--- cards/_components/GlobalSearch.vue
+++ cards/_components/GlobalSearch.vue
@@ -2,9 +2,12 @@
   <transparent-wrapper>
     <p-search-dropdown
       :model-value="allItems"
-      :placeholder="$t('search_placeholder')"
       :groups="dropdownGroups"
-      :filter="filter"
+      :placeholder="$t('search_placeholder')"
+      :filter="textFilter"
+      :is-loading="isLoading"
+      dont-filter-locally
+      @update:filter="onFilterChanged"
       @select="selectCallback"
       @search="searchCallback"
     />
@@ -12,6 +15,8 @@
 </template>
 
 <script>
+import { debounce, uniqBy } from 'lodash-es';
+import axios, { CancelToken } from 'axios';
 import common from '@/_mixins/common.js';
 import TransparentWrapper from '@/_components/TransparentWrapper.vue';
 import PSearchDropdown from '@/_components/SearchDropdown.vue';
@@ -27,51 +32,101 @@ export default {
     const people = this.cardData.data?.results?.people || [];
     const groups = this.cardData.data?.results?.groups || [];
 
-    const peopleItems = people.map((person) => ({
-      id: person.slug,
-      label: person.name,
-      image: this.getPersonPortrait(person),
-      selected: false,
-    }));
-
-    const groupItems = groups.map((group) => ({
-      id: group.slug,
-      label: group.name,
-      colorClass: 'xxx',
-      //   colorClass: `${group[0].person.group.acronym
-      //     .toLowerCase()
-      //     .replace(/[ +,]/g, '_')}-background`,
-      selected: false,
-    }));
-
-    const allItems = [...groupItems, ...peopleItems];
-
-    const dropdownGroups = [
-      {
-        id: 'groups',
-        label: this.$t('parties'),
-        items: groups.map((group) => group.slug),
-      },
-      ...groups.map((group) => ({
-        id: group.slug,
-        label: group.name,
-        items: people
-          .filter((person) => person?.group?.slug === group.slug)
-          .map((person) => person.slug),
-      })),
-    ];
-
-    const initialFilter = this.cardState.query || '';
+    const initialTextFilter = this.cardState.query || '';
 
     return {
       people,
       groups,
-      allItems,
-      dropdownGroups,
-      filter: initialFilter,
+      textFilter: initialTextFilter,
+      isLoading: false,
+      cancelRequest: null,
     };
   },
+  computed: {
+    allItems() {
+      const peopleItems = this.people.map((person) => ({
+        id: person.slug,
+        label: person.name,
+        image: this.getPersonPortrait(person),
+        selected: false,
+      }));
+
+      const groupItems = this.groups.map((group) => ({
+        id: group.slug,
+        label: group.name,
+        color: group.color,
+        selected: false,
+      }));
+
+      return [...groupItems, ...peopleItems];
+    },
+    dropdownGroups() {
+      const allGroups = uniqBy(
+        this.people.filter((p) => p.group).map((p) => p.group),
+        (g) => g.slug
+      );
+      return [
+        {
+          id: 'groups',
+          label: this.$t('parties'),
+          items: this.groups.map((group) => group.slug),
+        },
+        ...allGroups.map((group) => ({
+          id: group.slug,
+          label: group.name,
+          items: this.people
+            .filter((person) => person?.group?.slug === group.slug)
+            .map((person) => person.slug),
+        })),
+      ];
+    },
+    searchUrl() {
+      const url = new URL(this.cardData.url);
+      url.searchParams.set('text', this.textFilter);
+      return url.toString();
+    },
+  },
   methods: {
+    makeRequest(url) {
+      if (this.cancelRequest) {
+        this.cancelRequest();
+        this.cancelRequest = null;
+      }
+
+      return axios
+        .get(url, {
+          cancelToken: new CancelToken((c) => {
+            this.cancelRequest = c;
+          }),
+        })
+        .then(
+          (response) => {
+            this.cancelRequest = null;
+            return response;
+          },
+          (error) => {
+            this.cancelRequest = null;
+            throw error;
+          }
+        );
+    },
+    search: debounce(function searchVotes() {
+      this.isLoading = true;
+      // this.people = [];
+      // this.groups = [];
+      this.makeRequest(this.searchUrl).then((response) => {
+        this.people = response?.data?.results?.people || [];
+        this.groups = response?.data?.results?.groups || [];
+        this.isLoading = false;
+      });
+    }, 750),
+    onFilterChanged(newFilter) {
+      this.textFilter = newFilter;
+      this.isLoading = true;
+      // this.people = [];
+      // this.groups = [];
+      this.search();
+    },
     selectCallback(itemId) {
       // eslint-disable-next-line no-restricted-properties
       const { urls, siteMap: sm } = this.$root.$options.contextData;
