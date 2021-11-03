0c281f2adb31c973176e1ef3610eb4c64df38eaa
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-11-03 19:14:14 +0100

    Get ready to deploy

--- parlasite/config/index.js
+++ parlasite/config/index.js
@@ -4,9 +4,9 @@ const config = {
   port: 3066,
   serverTimeout: 120000,
   urls: {
-    cdn: 'https://parlassets-ljubljana.lb.djnd.si',
-    cards: 'https://parlacards.lb.djnd.si',
-    parladata: 'https://parladata.lb.djnd.si/v3',
+    cdn: 'https://parlassets-hrastnik.lb.djnd.si',
+    cards: 'https://parlacards-hrastnik.lb.djnd.si',
+    parladata: 'https://parladata-hrastnik.lb.djnd.si/v3',
   },
   locale: 'sl-obcine',
   leaderId: '261',
