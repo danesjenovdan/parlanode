86153cf4bf7e16fe681bf343cf06d36a542f76ff
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-11-04 12:52:52 +0100

    Get ready to deploy Lendava

--- parlasite/config/index.js
+++ parlasite/config/index.js
@@ -4,9 +4,9 @@ const config = {
   port: 3066,
   serverTimeout: 120000,
   urls: {
-    cdn: 'https://parlassets-hrastnik.lb.djnd.si',
-    cards: 'https://parlacards-hrastnik.lb.djnd.si',
-    parladata: 'https://parladata-hrastnik.lb.djnd.si/v3',
+    cdn: 'https://parlassets-lendava.lb.djnd.si',
+    cards: 'https://parlacards-lendava.lb.djnd.si',
+    parladata: 'https://parladata-lendava.lb.djnd.si/v3',
   },
   locale: 'sl-obcine',
   leaderId: '261',
