676a8e68e46910285268e701fe748b58740a9b1f
Filip Muki Dobranic <filipdobranic@gmail.com>
2021-11-24 22:04:23 +0100

    set sentry environment

--- build/card-entry-client.js
+++ build/card-entry-client.js
@@ -34,6 +34,7 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+  environment: "ljubljana",
 });
 
 app.use(i18n);
