d56c7994f546d6aa4a261e7ff7da4a6a90e2aae7
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-12-02 16:02:40 +0100

    add paging to misc/legislation

--- cards/_helpers/constants.js
+++ cards/_helpers/constants.js
@@ -1,5 +1,6 @@
 export const RIPPLE_DURATION = 1000;
 export const SPEECHES_PER_PAGE = 10;
 export const MEMBERS_PER_PAGE = 10;
+export const LEGISLATION_PER_PAGE = 10;
 export const PG_CLASSIFICATIONS = ['pg', 'club'];
 export const QUOTE_PADDING_LENGTH = 30;
