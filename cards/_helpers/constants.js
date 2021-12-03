ce14f7b666c16129a8cc7d44b41ff16f90d2c71d
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-12-03 10:30:09 +0100

    add pagination and server-side filtering to misc/sessions

--- cards/_helpers/constants.js
+++ cards/_helpers/constants.js
@@ -2,5 +2,6 @@ export const RIPPLE_DURATION = 1000;
 export const SPEECHES_PER_PAGE = 10;
 export const MEMBERS_PER_PAGE = 10;
 export const LEGISLATION_PER_PAGE = 10;
+export const SESSIONS_PER_PAGE = 10;
 export const PG_CLASSIFICATIONS = ['pg', 'club'];
 export const QUOTE_PADDING_LENGTH = 30;
