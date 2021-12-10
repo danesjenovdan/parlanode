18f3293b42882d9bb6c892294036e43404045a08
LepkoQQ <lepko.san@gmail.com>
2021-12-10 14:44:19 +0100

    fix eslint warnings in misc/sessions

--- cards/misc/sessions/card.vue
+++ cards/misc/sessions/card.vue
@@ -15,10 +15,7 @@
             only show working bodies dropdown if
             a non-root tab is selected
           -->
-          <div
-            v-if="currentFilter != 'root'"
-            class="col-md-6 filters"
-          >
+          <div v-if="currentFilter != 'root'" class="col-md-6 filters">
             <p-search-dropdown
               v-model="workingBodies"
               :placeholder="inputPlaceholder"
@@ -29,7 +26,8 @@
         </div>
       </div>
     </template>
-    <div v-if="isLoading" class="nalagalnik"></div> <!-- show loader while fetching results -->
+    <div v-if="isLoading" class="nalagalnik"></div>
+    <!-- show loader while fetching results -->
     <template v-else>
       <inner-card
         :columns="columns"
@@ -56,24 +54,13 @@ import { defaultHeaderConfig } from '@/_mixins/altHeaders.js';
 import { defaultOgImage } from '@/_mixins/ogImages.js';
 import PSearchDropdown from '@/_components/SearchDropdown.vue';
 import BlueButtonList from '@/_components/BlueButtonList.vue';
-import InnerCard from './InnerCard.vue';
 import Pagination from '@/_components/Pagination.vue';
 import { SESSIONS_PER_PAGE } from '@/_helpers/constants.js';
 // debounce is required for search field queries debouncing
 import { debounce } from 'lodash-es';
 // cancelableRequest is how we make requests to update the results
 import cancelableRequest from '@/_mixins/cancelableRequest.js';
-
-function enumerateClassification(classification) {
-  switch(classification) {
-    case 'root':
-      return -1;
-    case 'other':
-      return 1;
-    default:
-      return 0;
-  };
-}
+import InnerCard from './InnerCard.vue';
 
 export default {
   name: 'CardMiscSessions',
@@ -81,13 +68,9 @@ export default {
     InnerCard,
     PSearchDropdown,
     BlueButtonList,
-    Pagination
+    Pagination,
   },
-  mixins: [
-    common,
-    sessionListContextUrl,
-    cancelableRequest,
-  ],
+  mixins: [common, sessionListContextUrl, cancelableRequest],
   cardInfo: {
     doubleWidth: true,
   },
@@ -95,57 +78,58 @@ export default {
     const { cardState, cardData } = this.$root.$options.contextData;
 
     // check if we're embedded
-    const isEmbedded = (this.$root.$options.contextData.templateName === 'embed');
+    const isEmbedded = this.$root.$options.contextData.templateName === 'embed';
 
-    let {
-      results: results = [],
-      'sessions:pages': pages = 1,
-      'sessions:page': initialPage = 1,
-      'sessions:count': count = results.length ?? 0,
-      'sessions:per_page': perPage = SESSIONS_PER_PAGE,
-    } = cardData?.data || {};
+    const data = cardData?.data || {};
+    let results = data.results ?? [];
+    let pages = data['sessions:pages'] ?? 1;
+    const initialPage = data['sessions:page'] ?? 1;
+    const count = data['sessions:count'] ?? results.length ?? 0;
+    let perPage = data['sessions:per_page'] ?? SESSIONS_PER_PAGE;
     let page = Number(cardState?.page) || initialPage;
-    
+
     // if we're embedded we should override our current settings
     if (isEmbedded) {
       pages = Math.ceil(count / 5);
       perPage = 5;
-      page = (page * 2) - 1;
+      page = page * 2 - 1;
       results = results.slice(0, 5);
     }
 
-    // TODO explain why this is necessary
+    // create array that has a size equal to number of pages to cache loaded pages
     const sessionsPerPage = Array(pages);
     sessionsPerPage[initialPage - 1] = results;
 
     // group organizations by classifications
-    const classifications = cardData.data.organizations.filter((organization) => {
-      // this is to filter out organizations without a classification
-      return organization.classification !== null;
-    }).sort((a, b) => {
-      // this is to make sure root comes first and other comes last
-      switch(a.classification) {
-        case 'root':
-          return -1;
-        case 'other':
-          return 1;
-      };
-
-      switch(b.classification) {
-        case 'root':
-          return 1;
-        case 'other':
-          return -1;
-      };
-
-      return 0;
-    }).reduce((classifications, organization) => {
-      if (!Object.keys(classifications).includes(organization.classification)) {
-        classifications[organization.classification] = {};
-      }
-      classifications[organization.classification][organization.slug] = organization;
-      return classifications;
-    }, {});
+    const classifications = cardData.data.organizations
+      .filter((organization) => {
+        // this is to filter out organizations without a classification
+        return organization.classification != null;
+      })
+      .sort((a, b) => {
+        // make sure `root` comes first and `other` comes last
+        if (a.classification !== b.classification) {
+          if (a.classification === 'root') {
+            return -1;
+          }
+          if (a.classification === 'other') {
+            return 1;
+          }
+          if (b.classification === 'root') {
+            return 1;
+          }
+          if (b.classification === 'other') {
+            return -1;
+          }
+        }
+        return 0;
+      })
+      .reduce((returnValue, organization) => {
+        const { classification, slug } = organization;
+        returnValue[classification] = returnValue[classification] ?? {};
+        returnValue[classification][slug] = organization;
+        return returnValue;
+      }, {});
 
     // create tabs
     const tabs = Object.keys(classifications).map((classificationKey) => {
@@ -162,7 +146,7 @@ export default {
       sessions: cardData?.data?.results,
       workingBodies: [],
       filters: tabs.map((tab) => ({ label: tab.title, id: tab.id })),
-      currentSort: 'date',
+      currentSort: 'start_time',
       currentSortOrder: 'desc',
       currentFilter: cardState?.filters || tabs[0].id,
       headerConfig: defaultHeaderConfig(this),
@@ -193,20 +177,15 @@ export default {
       if (this.currentWorkingBodies.length > 0) {
         url.searchParams.set(
           'organizations',
-          this.currentWorkingBodies.map(
-            (workingBodySlug) => workingBodySlug.split('-')[0]
-          ).join(',')
+          this.currentWorkingBodies
+            .map((workingBodySlug) => workingBodySlug.split('-')[0])
+            .join(',')
         );
       }
 
       // set sort param
       const sortPrefix = this.currentSortOrder === 'desc' ? '-' : '';
-      let sort = 'start_time';
-      switch (this.currentSort) {
-        case 'name':
-          sort = 'name';
-          break;
-      };
+      const sort = this.currentSort;
       url.searchParams.set('order_by', `${sortPrefix}${sort}`);
 
       return url.toString();
@@ -216,7 +195,7 @@ export default {
       return [
         { id: 'image', label: '', additionalClass: 'image' },
         { id: 'name', label: this.$t('title'), additionalClass: 'wider name' },
-        { id: 'date', label: this.$t('date') },
+        { id: 'start_time', label: this.$t('date') },
         // TODO this should be properly optional instead of commented out
         // {
         //   id: 'updated',
@@ -270,9 +249,7 @@ export default {
       this.searchSessionsImmediate();
 
       // get current tab
-      const tab = this.tabs.filter((tab) => {
-        return tab.id === argument;
-      })[0];
+      const tab = this.tabs.find((t) => t.id === argument);
 
       // map all organization slugs in the tab to
       // proper objects that PSearchDropdown can consume
@@ -295,8 +272,7 @@ export default {
       this.makeRequest(this.searchUrl).then((response) => {
         this.count = response?.data?.['sessions:count'];
         this.page = response?.data?.['sessions:page'];
-        this.sessionsPerPage[this.page - 1] =
-          response?.data?.results || [];
+        this.sessionsPerPage[this.page - 1] = response?.data?.results || [];
         this.isLoading = false;
       });
     },
@@ -311,8 +287,7 @@ export default {
       if (!this.sessionsPerPage[newPage - 1]) {
         this.isLoading = true;
         this.makeRequest(this.searchUrl).then((response) => {
-          this.sessionsPerPage[newPage - 1] =
-            response?.data?.results || [];
+          this.sessionsPerPage[newPage - 1] = response?.data?.results || [];
           this.isLoading = false;
         });
       }
