134a4e929e51fe51fe909032c0b9bd1b10e2685a
Tomaz <tomazkunst@gmail.com>
2022-01-18 16:49:10 +0100

    deployment

--- parlasite/config/index.js
+++ parlasite/config/index.js
@@ -6,9 +6,9 @@ const config = {
   port: 3066,
   serverTimeout: 120000,
   urls: {
-    cdn: 'https://parlassets-ljubljana.lb.djnd.si',
-    cards: 'https://parlacards.lb.djnd.si',
-    parladata: 'https://parladata.lb.djnd.si/v3',
+    cdn: 'https://parlassets-slovenija.lb.djnd.si',
+    cards: 'https://parlacards-slovenija.lb.djnd.si',
+    parladata: 'https://parladata-slovenija.lb.djnd.si/v3',
   },
   locale: 'sl-obcine',
   leaderId: '261',
