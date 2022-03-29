f01532de109b02c06bae4049d7ce5c9a43a887ef
LepkoQQ <lepko.san@gmail.com>
2022-03-29 18:15:44 +0200

    fix building parlacards with new monorepo

--- parlacards/build/scss-functions.js
+++ parlacards/build/scss-functions.js
@@ -1,5 +1,9 @@
 import sass from 'sass';
 
+if (!process.env.VITE_PARLASSETS_URL) {
+  throw new Error('process.env.VITE_PARLASSETS_URL is not defined!');
+}
+
 export default {
   'get-parlassets-url()': () => {
     return new sass.types.String(process.env.VITE_PARLASSETS_URL ?? '');
