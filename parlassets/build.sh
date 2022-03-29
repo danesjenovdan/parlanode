346e2cfecfb932d398e5cd5787af7b7dd113f156
LepkoQQ <lepko.san@gmail.com>
2022-03-29 18:25:04 +0200

    change building parlassets so it respects .dockerignore

--- parlassets/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
-
-# BUILD AND PUBLISH parlassets-ljubljana
-sudo docker build -f Dockerfile -t parlassets-ljubljana:latest .
-sudo docker tag parlassets-ljubljana:latest rg.fr-par.scw.cloud/djnd/parlassets-ljubljana:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/parlassets-ljubljana:latest
