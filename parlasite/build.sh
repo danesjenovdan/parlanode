b2feeeb1309f5fbe337a44ce8208f9e21c6a8b7c
LepkoQQ <lepko.san@gmail.com>
2022-04-08 11:25:04 +0200

    fix build, docker, kustomize and github workflows for merged in parlasite

--- parlasite/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
-
-# BUILD AND PUBLISH PRAVNA MREZA
-sudo docker build -f Dockerfile -t parlasite:latest .
-sudo docker tag parlasite:latest rg.fr-par.scw.cloud/djnd/parlasite:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/parlasite:latest
