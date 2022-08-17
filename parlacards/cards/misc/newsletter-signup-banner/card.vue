c2514472fb51b3aedcddbecf205cc47ec614efd4
LepkoQQ <lepko.san@gmail.com>
2022-08-17 20:08:11 +0200

    add newsletter signup cards

---
+++ parlacards/cards/misc/newsletter-signup-banner/card.vue
@@ -0,0 +1,29 @@
+<template>
+  <transparent-wrapper>
+    <newsletter-signup type="banner" />
+  </transparent-wrapper>
+</template>
+
+<script>
+import common from '@/_mixins/common.js';
+import TransparentWrapper from '@/_components/TransparentWrapper.vue';
+import NewsletterSignup from '@/_components/NewsletterSignup.vue';
+
+export default {
+  name: 'CardMiscNewsletterSignup',
+  components: {
+    TransparentWrapper,
+    NewsletterSignup,
+  },
+  mixins: [common],
+  cardInfo: {
+    doubleWidth: true,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+:deep(.card-content) {
+  padding: 0;
+}
+</style>
