f38dcabfdc0e0e04dce7fc4fbbb600375710fc66
LepkoQQ <lepko.san@gmail.com>
2022-08-19 11:56:40 +0200

    add search/minutes card

---
+++ parlacards/cards/search/minutes/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <search-minutes type="search" />
+</template>
+
+<script>
+import SearchMinutes from '@/_components/SearchMinutes.vue';
+
+export default {
+  name: 'CardSearchSpeeches',
+  components: {
+    SearchMinutes,
+  },
+  cardInfo: {
+    doubleWidth: true,
+  },
+};
+</script>
