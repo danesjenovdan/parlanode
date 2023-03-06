884e164d065fa8f0c4002d759362ec4353e41ec4
LepkoQQ <lepko.san@gmail.com>
2023-03-06 18:08:34 +0100

    add bosna sentry environment

--- parlacards/build/card-entry-client.js
+++ parlacards/build/card-entry-client.js
@@ -33,6 +33,7 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+  environment: 'bosna',
   // Ignore some external errors, such as facebook,
   // twitter, slack bots or link crawlers and scanners.
   ignoreErrors: [
