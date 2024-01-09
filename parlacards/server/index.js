750e19abbc99e90927bd6ea9330ec52665509119
LepkoQQ <lepko.san@gmail.com>
2024-01-09 16:40:19 +0100

    itegrate sentry with fastify

--- parlacards/server/index.js
+++ parlacards/server/index.js
@@ -3,17 +3,17 @@ import { resolve } from 'path';
 import { fastify as createFastify } from 'fastify';
 import fastifyStatic from 'fastify-static';
 import fastifyCors from 'fastify-cors';
-import * as Sentry from '@sentry/node';
+import fastifySentry from '@immobiliarelabs/fastify-sentry';
 import { renderCard } from './render-card.js';
 
-Sentry.init({
+const fastify = createFastify({ logger: true, ignoreTrailingSlash: true });
+
+fastify.register(fastifySentry, {
   dsn: 'https://07dc842d53be467b8f158c93984a3fb9@o1076834.ingest.sentry.io/6080015',
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
 });
-
-const fastify = createFastify({ logger: true, ignoreTrailingSlash: true });
 
 fastify.register(fastifyCors, {
   origin: '*',
